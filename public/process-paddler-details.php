4735d18f62643dbea4319884e6b1d45a1a757ef9
Richard Crooks <email@rocrooks.co.uk>
2019-04-18 17:20:13 +0100

    Change paddler processing engine to process single paddler

--- public/process-paddler-details.php
+++ public/process-paddler-details.php
@@ -1,26 +1,18 @@
 <?php
 include_once 'required-functions.php';
 
-foreach ($paddlerdetails as $paddlerkey=>$paddler)
+//Flag for if the result is a finish or not
+if ($paddler['NR'] == '')
   {
-  //Flag for if the result is a finish or not
-  if ($paddler['NR'] == '')
-    {
-    $paddler['Result'] = true;
-    $paddler['Time'] = secstohms($secs);
-    }
-  else
-    {
-    //A no result line
-    $paddler['Result'] = false;
-    $paddler['Time'] = $paddler['NR'];
-    }
-
-  unset($paddler['NR']);
-  $paddlerdetails[$paddlerkey] = $paddler;
+  $paddler['Result'] = true;
+  $paddler['Time'] = secstohms($secs);
+  }
+else
+  {
+  //A no result line
+  $paddler['Result'] = false;
+  $paddler['Time'] = $paddler['NR'];
   }
 
-usort($paddlerdetails,'sortfinishers');
-
-print_r($paddlerdetails);
+unset($paddler['NR']);
 ?>
