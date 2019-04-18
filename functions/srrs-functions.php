d6b95d740028f384739d2d4db707ad4fab853965
Richard Crooks <email@rocrooks.co.uk>
2019-04-18 14:23:07 +0100

    Add functions to convert between time formats

--- functions/srrs-functions.php
+++ functions/srrs-functions.php
@@ -47,4 +47,56 @@ function sortraceslist($a,$b)
   return ($a < $b) ? -1 : 1;
 	}
 //---FunctionBreak---
+/*Convert a time in seconds to a time in HH:MM:SS
+
+$time is the time in seconds
+
+Output is the time in HH:MM:SS format.*/
+//---DocumentationBreak---
+function secstohms($time)
+	{
+	//Format time in seconds for display
+	$hours = floor($time/3600);
+	$mins = (floor($time/60))-($hours*60);
+	$seconds = $time%60;
+	$hms = $seconds;
+	if (($mins > 0) OR ($hours > 0))
+		{
+		if ($seconds < 10)
+			$hms = "0" . $hms;
+		$hms = $mins . ":" . $hms;
+		if ($hours > 0)
+			{
+			if ($mins < 10)
+				$hms = "0" . $hms;
+
+			$hms = $hours . ":" . $hms;
+			}
+		}
+
+	Return $hms;
+	}
+//---FunctionBreak---
+/*Convert a time in HH:MM:SS to a time in seconds
+
+$time is the time in HH:MM:SS
+
+Output is the time in seconds.*/
+//---DocumentationBreak---
+function hmstosecs($time)
+	{
+	//Convert display time to a time in seconds
+	$time = explode(":",$time);
+
+	$time = array_reverse($time);
+
+	$seconds = $time[0];
+	if (isset($time[1]) == true)
+		$seconds = $seconds+($time[1]*60);
+	if (isset($time[2]) == true)
+		$seconds = $seconds+($time[2]*60*60);
+
+	Return $seconds;
+	}
+//---FunctionBreak---
 ?>
