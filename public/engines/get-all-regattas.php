0e5c77ee1abbb568b4147800e27b2fd4c6e6a322
Richard Crooks <email@rocrooks.co.uk>
2019-04-25 22:31:55 +0100

    Allow filtering by club and paddler

--- public/engines/get-all-regattas.php
+++ public/engines/get-all-regattas.php
@@ -1,15 +1,37 @@
 <?php
 include_once 'required-functions.php';
 
-//Get regatta details from regatta database
+//Default SQL to retrieve regattas
 $regattasdetailssql = "SELECT `Key`, `Date`, `Days`, `Name` FROM `regattas`";
-$regattasdetailsresults = dbprepareandexecute($srrsdblink,$regattasdetailssql,array());
+
+//Make constraints if there are club or paddler specified
+if (($club != "") OR ($paddler != ""))
+  {
+  include 'filter-paddler-race-ids.php';
+
+  $racesfilter = makesqlrange($paddlerraceids,"Key");
+
+  //Search for regatta IDs that are in the races
+  $regattaidssql = "SELECT DISTINCT `Regatta` FROM `races` WHERE " . $racesfilter['SQLText'];
+  $regattaidsconstraints = $racesfilter['SQLValues'];
+  $regattaids = dbprepareandexecute($srrsdblink,$regattaidssql,$regattaidsconstraints);
+  $regattaids = resulttocolumn($regattaids,"Regatta");
+
+  //Filter regatta list
+  $regattasfilter = makesqlrange($regattaids,"Key");
+  $regattasdetailssql = $regattasdetailssql . " WHERE " . $regattasfilter['SQLText'];
+  $regattalistsqlconstraints = $regattasfilter['SQLValues'];
+  }
+else
+  $regattalistsqlconstraints = array();
+
+
+//Get regatta details from regatta database
+$regattasdetailsresults = dbprepareandexecute($srrsdblink,$regattasdetailssql,$regattalistsqlconstraints);
 
 foreach($regattasdetailsresults as $regattadetailskeys=>$regattadetailsline)
   {
   include 'process-regatta-details.php';
   $regattasdetailsresults[$regattadetailskeys] = $regattadetailsline;
   }
-
-print_r($regattasdetailsresults);
 ?>
