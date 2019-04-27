761950af1a21cfe52d142f8a98385bd52dc4250a
Richard Crooks <email@rocrooks.co.uk>
2019-04-27 22:51:06 +0100

    Engine to count paddlers

---
+++ public/engines/count-paddlers.php
@@ -0,0 +1,50 @@
+<?php
+include_once 'required-functions.php';
+
+$jsv = "S";
+$mw = "W";
+$ck = "K";
+$raceids = array();
+
+//Default count
+$paddlerseatssql = "SELECT COUNT(*) FROM `paddlers` ";
+
+$paddlersqltext = array();
+$paddlerconstraints = array();
+
+//Add constraints to paddler query if specified
+//Raceids are used to select races (and from that paddlers) that meet boat size, distance or race class
+if ($jsv != "")
+  {
+  array_push($paddlersqltext,"`JSV` = ?");
+  array_push($paddlerconstraints,$jsv);
+  }
+if ($mw != "")
+  {
+  array_push($paddlersqltext,"`MW` = ?");
+  array_push($paddlerconstraints,$mw);
+  }
+if ($ck != "")
+  {
+  array_push($paddlersqltext,"`CK` = ?");
+  array_push($paddlerconstraints,$ck);
+  }
+if (count($raceids) > 0)
+  {
+  $raceidssql = makesqlrange($raceids,"Race");
+  array_push($paddlersqltext,$raceidssql['SQLText']);
+  array_push($paddlerconstraints,$raceidssql['SQLValues']);
+  }
+
+if (count($paddlersqltext) > 0)
+  {
+  $paddlersqltext = implode(" AND ",$paddlersqltext);
+  $paddlerseatssql = $paddlerseatssql . " WHERE " . $paddlersqltext;
+  }
+
+//Count the number of races of this type
+$paddlerscount = dbprepareandexecute($srrsdblink,$paddlerseatssql,$paddlerconstraints);
+$paddlerscount = $paddlerscount[0]['COUNT(*)'];
+
+echo $paddlerscount . "<br>";
+?>
