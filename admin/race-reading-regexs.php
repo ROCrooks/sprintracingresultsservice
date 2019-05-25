71efd9d845b1d52c3ed851eeddcb99e740e2c0bc
Richard Crooks <email@rocrooks.co.uk>
2019-05-25 20:26:14 +0100

    Regular expressions for race features

---
+++ admin/race-reading-regexs.php
@@ -0,0 +1,13 @@
+<?php
+$regex['boats'] = '(^[kcv][124])i';
+$regex['distance'] = '([0-9][km])i';
+$regex['round'] = '([s*f*h*][0-9])';
+$regex['raceno'] = '(^[0-9]$)';
+$regex['paddler'] = '([a-z])i';
+$regex['positionandlane'] = '([0-9] [0-9])';
+$regex['positionorlane'] = '([0-9] [A-Z])';
+$regex['pagedetails'] = '(page [0-9] of [0-9])i';
+$regex['regulartime'] = '([0-9]:[0-9].[0-9])';
+$regex['shorttime'] = '([0-9].[0-9])';
+$regex['longtime'] = '([0-9]:[0-9]:[0-9])';
+?>
