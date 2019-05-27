1313f8e8692370e2953afe104a08f92e9bf04aff
Richard Crooks <email@rocrooks.co.uk>
2019-05-27 21:01:08 +0100

    Engine for checking the race for database import

---
+++ admin/engines/check-race-import.php
@@ -0,0 +1,93 @@
+<?php
+$racedetails = array("Distance"=>"200","defCK"=>"K","Boat"=>"1","Draw"=>"1","Round"=>"F","RaceName"=>"MEN OPEN (INC U23) K","defMW"=>"M","defJSV"=>"S");
+$allpaddlerdetails = array();
+$allpaddlerdetails[0] = array("Time"=>"35.41","NR"=>"","Position"=>"1","Lane"=>"5","Club"=>"WEY","Crew"=>"L. HEATH","JSV"=>"S","MW"=>"M","CK"=>"K");
+$allpaddlerdetails[1] = array("Time"=>"35.77","NR"=>"","Position"=>"2","Lane"=>"6","Club"=>"SOR","Crew"=>"J. SCHOFIELD","JSV"=>"S","MW"=>"M","CK"=>"K");
+$allpaddlerdetails[2] = array("Time"=>"36.09","NR"=>"","Position"=>"3","Lane"=>"4","Club"=>"SPS","Crew"=>"L. FLETCHER","JSV"=>"S","MW"=>"M","CK"=>"K");
+$allpaddlerdetails[3] = array("Time"=>"36.58","NR"=>"","Position"=>"4","Lane"=>"3","Club"=>"SPS","Crew"=>"I. JAMES","JSV"=>"S","MW"=>"M","CK"=>"K");
+$allpaddlerdetails[4] = array("Time"=>"37.01","NR"=>"","Position"=>"5","Lane"=>"7","Club"=>"NOT","Crew"=>"S. NAFTANAILA","JSV"=>"S","MW"=>"M","CK"=>"K");
+$allpaddlerdetails[5] = array("Time"=>"37.14","NR"=>"","Position"=>"6","Lane"=>"2","Club"=>"EAL","Crew"=>"T. THOMSON","JSV"=>"S","MW"=>"M","CK"=>"K");
+$allpaddlerdetails[6] = array("Time"=>"37.56","NR"=>"","Position"=>"7","Lane"=>"8","Club"=>"CDF","Crew"=>"M. ROBINSON","JSV"=>"S","MW"=>"M","CK"=>"K");
+$allpaddlerdetails[7] = array("Time"=>"37.67","NR"=>"","Position"=>"8","Lane"=>"9","Club"=>"RDG","Crew"=>"D. ATKINS","JSV"=>"S","MW"=>"M","CK"=>"K");
+$allpaddlerdetails[8] = array("Time"=>"38.67","NR"=>"","Position"=>"9","Lane"=>"1","Club"=>"LEA","Crew"=>"N. DEMBELE","JSV"=>"S","MW"=>"M","CK"=>"K");
+
+//A list of errors
+$errorlist = array();
+
+//Check that the distance is legal
+$legaldistances = array(200,500,1000,2500,3000,5000,6000,10000);
+if (in_array($racedetails['Distance'],$legaldistances) === false)
+  {
+  $raceerror = true;
+  array_push($errorlist,"Invalid race distance specified");
+  }
+
+//Check that the round is legal
+$legalrounds = array("H","QF","SF","F");
+if (in_array($racedetails['Round'],$legalrounds) === false)
+  {
+  $raceerror = true;
+  array_push($errorlist,"Invalid round specified");
+  }
+
+//Check that the draw is legal
+if (($racedetails['Round'] != "F") AND ($racedetails['Round'] == 0))
+  {
+  $raceerror = true;
+  array_push($errorlist,"Draw missing from a round that needs one");
+  }
+
+$legalboats = array(1,2,4);
+if (in_array($racedetails['Boat'],$legalboats) === false)
+  {
+  $raceerror = true;
+  array_push($errorlist,"Invalid boat size specified");
+  }
+
+$legaljsvs = str_split("JSV");
+$legalmw = str_split("MW");
+$legalck = str_split("CKVTP");
+$legalnrs = array("DSQ","ERR","DNS","DNF","???");
+foreach ($allpaddlerdetails as $paddlerdetails)
+  {
+  //Check JSV is legal
+  if (in_array($paddlerdetails['JSV'],$legaljsvs) === false)
+    {
+    $raceerror = true;
+    array_push($errorlist,"Invalid JSV specified");
+    }
+
+  //Check MW is legal
+  if (in_array($paddlerdetails['MW'],$legalmw) === false)
+    {
+    $raceerror = true;
+    array_push($errorlist,"Invalid MW specified");
+    }
+
+  //Check CK is legal
+  if (in_array($paddlerdetails['CK'],$legalck) === false)
+    {
+    $raceerror = true;
+    array_push($errorlist,"Invalid CK specified");
+    }
+
+  //Check NR is legal
+  if ((in_array($paddlerdetails['NR'],$legalck) === false) AND ($paddlerdetails['Time'] == 0))
+    {
+    $raceerror = true;
+    array_push($errorlist,"Invalid no result code specified");
+    }
+
+  if ((strlen($paddlerdetails['Club']) == 3) OR ((strlen($paddlerdetails['Club']) != 7) AND ($racedetails['Boat'] == 2)) OR ((strlen($paddlerdetails['Club']) != 15) AND ($racedetails['Boat'] == 4)))
+    {
+    $raceerror = true;
+    array_push($errorlist,"Invalid club code specified");
+    }
+  }
+
+print_r($racedetails);
+echo "<br>";
+print_r($allpaddlerdetails);
+echo "<br>";
+print_r($errorlist);
+?>
