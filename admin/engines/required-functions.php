0293fd6d2c67f03aa3d2f1b7f1af85170310f842
Richard Crooks <email@rocrooks.co.uk>
2019-05-27 22:43:09 +0100

    Include required files

---
+++ admin/engines/required-functions.php
@@ -0,0 +1,14 @@
+<?php
+$files = array("srrs","userinput","db","array");
+
+//Include each required functions file
+foreach ($files as $file)
+  {
+  $url = "../../public/functions/" . $file . "-functions.php";
+  include_once $url;
+  }
+
+//Create database connection
+include_once '../../../dbconnect-function.php';
+$srrsdblink = createdbconnection("SRRS");
+?>
