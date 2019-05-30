afa76c5e6d3e00ce81bfb878293e59fef7b2d76e
Richard Crooks <email@rocrooks.co.uk>
2019-05-30 17:49:43 +0100

    SQL queries for prepared statements ready

--- admin/engines/import-race-db.php
+++ admin/engines/import-race-db.php
@@ -3,7 +3,12 @@ include_once 'required-functions.php';
 
 $regattaid = 1000;
 
+//SQL statements required for inserting races
 $paddlertimesql = "INSERT INTO `paddlers` (`Race`, `Position`, `Lane`, `Crew`, `Club`, `Time`, `JSV`, `MW`, `CK`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 $paddlernrsql = "INSERT INTO `paddlers` (`Race`, `Position`, `Lane`, `Crew`, `Club`, `NR`, `JSV`, `MW`, `CK`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-$racesql = "INSERT INTO `races` (`Regatta`, `Class`, `Boat`, `Dist`, `R`, `D`) VALUES (?, ?, ?, ?, ?, ?)"
+$racesql = "INSERT INTO `races` (`Regatta`, `Class`, `Boat`, `Dist`, `R`, `D`, `Set`) VALUES (?, ?, ?, ?, ?, ?, ?)";
+$findclasssql = "SELECT * FROM `autoclasses` WHERE `Class` = ?";
+$insertclasssql = "INSERT INTO `classes` (`Race`, `JSV`, `MW`, `CK`, `Spec`, `Abil`, `Ages`, `FreeText`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+
 ?>
