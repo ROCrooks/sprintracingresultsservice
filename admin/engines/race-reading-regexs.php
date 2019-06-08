6dd51d1abb8c2626a50ceb673fa8757319ae6371
Richard Crooks <email@rocrooks.co.uk>
2019-06-08 14:55:05 +0100

    Update time regexes

--- admin/engines/race-reading-regexs.php
+++ admin/engines/race-reading-regexs.php
@@ -7,7 +7,7 @@ $regex['paddler'] = '([a-z])i';
 $regex['positionandlane'] = '(^[0-9] [0-9] )';
 $regex['positionorlane'] = '(^[0-9] [0-9A-Z][0-9A-Z][0-9A-Z] )';
 $regex['pagedetails'] = '(page [0-9] of [0-9])i';
-$regex['regulartime'] = '([0-9]:[0-9].[0-9])';
-$regex['shorttime'] = '([0-9].[0-9])';
-$regex['longtime'] = '([0-9]:[0-9]:[0-9])';
+$regex['regulartime'] = '([0-9]:[0-5][0-9].[0-9][0-9])';
+$regex['shorttime'] = '([2-5][0-9].[0-9][0-9])';
+$regex['longtime'] = '([0-9]:[0-5][0-9]:[0-9][0-9])';
 ?>
