4be1c730d9093189541e2c948b1944da9fc8f37e
Richard Crooks <email@rocrooks.co.uk>
2019-06-22 13:02:58 +0100

    Make required file lookups use absolute path

--- admin/engines/required-functions.php
+++ admin/engines/required-functions.php
@@ -1,14 +1,23 @@
 <?php
 $files = array("srrs","userinput","db","array");
 
+//Get current directory
+//This makes the location independent of the location of the calling script
+$directory = dirname(__DIR__);
+$directory = str_replace("admin","public/functions",$directory);
+
 //Include each required functions file
 foreach ($files as $file)
   {
-  $url = "../public/functions/" . $file . "-functions.php";
+  $url = $directory . "/" . $file . "-functions.php";
   include_once $url;
   }
 
+//Change the directory to where the database script is
+$directory = str_replace("Git/public/functions","",$directory);
+
 //Create database connection
-include_once '../../dbconnect-function.php';
+include_once $directory . '/dbconnect-function.php';
 $srrsdblink = createdbconnection("SRRS");
+
 ?>
