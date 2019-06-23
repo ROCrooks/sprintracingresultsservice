b29d5ecbe16920cc4ec44e89043aec21ce756e3a
Richard Crooks <email@rocrooks.co.uk>
2019-06-23 22:46:08 +0100

    Initialise event metrics engine with JOIN query

---
+++ public/engines/all-event-paddlers-engine.php
@@ -0,0 +1,47 @@
+<?php
+include_once 'required-functions.php';
+
+$year = "";
+$club = "";
+$jsv = "";
+
+//Get regatta metrics
+$endconstraints = array();
+$geteventanalyticssql = "SELECT
+    `Club`,
+    `Crew`,
+    `Dist`,
+    `Boat`,
+    `JSV`,
+    `MW`,
+    `CK`,
+    `Year`
+FROM
+    `paddlers` p
+LEFT JOIN `races` r ON r.`Key` = p.`Race`
+LEFT JOIN `regattas` g ON g.`Key` = r.`Regatta`
+WHERE `MW` = ? AND `CK` = ? AND `Boat` = ? AND `Dist` = ? AND `NR` = ''";
+if ($year != "")
+  {
+  $geteventanalyticssql = $geteventanalyticssql . " AND `Year` = ?";
+  array_push($endconstraints,$year);
+  }
+if ($club != "")
+  {
+  $geteventanalyticssql = $geteventanalyticssql . " AND `Club` = ?";
+  array_push($endconstraints,$club);
+  }
+if ($jsv != "")
+  {
+  $geteventanalyticssql = $geteventanalyticssql . " AND `JSV` = ?";
+  array_push($endconstraints,$jsv);
+  }
+"ORDER BY
+    p.`Key` ASC";
+$querydata = array("M","K",1,500);
+$querydata = array_merge($querydata,$endconstraints);
+
+$eventdata = dbprepareandexecute($srrsdblink,$geteventanalyticssql,$querydata);
+
+print_r($eventdata);
+?>
