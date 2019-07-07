30bf6f8c3b4e82e5bd682b8c1da9abfc98c4405d
Richard Crooks <email@rocrooks.co.uk>
2019-07-07 21:53:19 +0100

    Handle errors caused by invalid race ID

--- public/engines/process-race-details.php
+++ public/engines/process-race-details.php
@@ -1,72 +1,87 @@
 <?php
 include_once 'required-functions.php';
 
-//Format name of round and draw
-if (($racesqlresultline['R'] == "H") OR ($racesqlresultline['R'] == 1))
+if (is_array($racesqlresultline) == true)
   {
-  $rounddraw = "Heat";
+  //Format name of round and draw
+  if (($racesqlresultline['R'] == "H") OR ($racesqlresultline['R'] == 1))
+    {
+    $rounddraw = "Heat";
+    $roundno = 1;
+    }
+  if (($racesqlresultline['R'] == "QF") OR ($racesqlresultline['R'] == "Q") OR ($racesqlresultline['R'] == 2))
+    {
+    $rounddraw = "Quarter-Final";
+    $roundno = 2;
+    }
+  if (($racesqlresultline['R'] == "SF") OR ($racesqlresultline['R'] == "S") OR ($racesqlresultline['R'] == 3))
+    {
+    $rounddraw = "Semi-Final";
+    $roundno = 3;
+    }
+  if (($racesqlresultline['R'] == "F") OR ($racesqlresultline['R'] == 4))
+    {
+    $rounddraw = "Final";
+    $roundno = 4;
+    }
+  else
+    {
+    $rounddraw = "";
+    $roundno = 0;
+    }
+
   if ($racesqlresultline['D'] > 0)
     $rounddraw = $rounddraw . " " . $racesqlresultline['D'];
-  $roundno = 1;
+
+  //Format distance
+  if ($racesqlresultline['Dist'] <= 1000)
+    $distance = $racesqlresultline['Dist'] . "m";
+  if ($racesqlresultline['Dist'] > 1000)
+    $distance = $racesqlresultline['Dist']/1000 . "km";
+
+  //Format the class of the race from the classes table
+  $getclassdetailssql = "SELECT `JSV`, `MW`, `CK`, `Abil`, `Spec`, `Ages`, `FreeText` FROM `classes` WHERE `Race` = ? ";
+
+  //Prepare an SQL statement to run multiple times, as this engine can be called in a loop
+  if (isset($retainedclassdetailssql) == false)
+    {
+    $retainedclassdetailssql = $getclassdetailssql;
+    $classdetailsstmt = dbprepare($srrsdblink,$retainedclassdetailssql);
+    }
+  elseif ($retainedclassdetailssql != $getclassdetailssql)
+    {
+    $retainedclassdetailssql = $getclassdetailssql;
+    $classdetailsstmt = dbprepare($srrsdblink,$retainedclassdetailssql);
+    }
+
+  $classdetails = dbexecute($classdetailsstmt,$raceid);
+  //$classdetails = dbprepareandexecute($srrsdblink,$getclassdetailssql,$raceid);
+  $boatsize = $racesqlresultline['Boat'];
+  include 'format-class.php';
+
+  //Format name of race
+  $racename = $raceclass . " " . $distance . " " . $rounddraw;
+
+  //Race details into an array
+  $racedetails = array();
+  $racedetails['Key'] = $raceid;
+  $racedetails['Regatta'] = $racesqlresultline['Regatta'];
+  $racedetails['Name'] = $racename;
+  $racedetails['Class'] = $raceclass;
+  $racedetails['Distance'] = $racesqlresultline['Dist'];
+  $racedetails['Round'] = $roundno;
+  $racedetails['Draw'] = $racesqlresultline['D'];
   }
-if (($racesqlresultline['R'] == "QF") OR ($racesqlresultline['R'] == "Q") OR ($racesqlresultline['R'] == 2))
+else
   {
-  $rounddraw = "Quarter-Final";
-  if ($racesqlresultline['D'] > 0)
-    $rounddraw = $rounddraw . " " . $racesqlresultline['D'];
-  $roundno = 2;
+  //Race details into an array
+  $racedetails = array();
+  $racedetails['Key'] = $raceid;
+  $racedetails['Regatta'] = 0;
+  $racedetails['Name'] = "";
+  $racedetails['Class'] = "";
+  $racedetails['Distance'] = 0;
+  $racedetails['Round'] = 0;
+  $racedetails['Draw'] = 0;
   }
-if (($racesqlresultline['R'] == "SF") OR ($racesqlresultline['R'] == "S") OR ($racesqlresultline['R'] == 3))
-  {
-  $rounddraw = "Semi-Final";
-  if ($racesqlresultline['D'] > 0)
-    $rounddraw = $rounddraw . " " . $racesqlresultline['D'];
-  $roundno = 3;
-  }
-if (($racesqlresultline['R'] == "F") OR ($racesqlresultline['R'] == 4))
-  {
-  $rounddraw = "Final";
-  if ($racesqlresultline['D'] > 0)
-    $rounddraw = $rounddraw . " " . $racesqlresultline['D'];
-  $roundno = 4;
-  }
-
-//Format distance
-if ($racesqlresultline['Dist'] <= 1000)
-  $distance = $racesqlresultline['Dist'] . "m";
-if ($racesqlresultline['Dist'] > 1000)
-  $distance = $racesqlresultline['Dist']/1000 . "km";
-
-//Format the class of the race from the classes table
-$getclassdetailssql = "SELECT `JSV`, `MW`, `CK`, `Abil`, `Spec`, `Ages`, `FreeText` FROM `classes` WHERE `Race` = ? ";
-
-//Prepare an SQL statement to run multiple times, as this engine can be called in a loop
-if (isset($retainedclassdetailssql) == false)
-  {
-  $retainedclassdetailssql = $getclassdetailssql;
-  $classdetailsstmt = dbprepare($srrsdblink,$retainedclassdetailssql);
-  }
-elseif ($retainedclassdetailssql != $getclassdetailssql)
-  {
-  $retainedclassdetailssql = $getclassdetailssql;
-  $classdetailsstmt = dbprepare($srrsdblink,$retainedclassdetailssql);
-  }
-
-$classdetails = dbexecute($classdetailsstmt,$raceid);
-//$classdetails = dbprepareandexecute($srrsdblink,$getclassdetailssql,$raceid);
-$boatsize = $racesqlresultline['Boat'];
-include 'format-class.php';
-
-//Format name of race
-$racename = $raceclass . " " . $distance . " " . $rounddraw;
-
-//Race details into an array
-$racedetails = array();
-$racedetails['Key'] = $raceid;
-$racedetails['Regatta'] = $racesqlresultline['Regatta'];
-$racedetails['Name'] = $racename;
-$racedetails['Class'] = $raceclass;
-$racedetails['Distance'] = $racesqlresultline['Dist'];
-$racedetails['Round'] = $roundno;
-$racedetails['Draw'] = $racesqlresultline['D'];
 ?>
