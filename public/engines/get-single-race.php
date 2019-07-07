30210c961884d224ef0a60c38de6f2a9b525dc06
Richard Crooks <email@rocrooks.co.uk>
2019-07-07 21:54:06 +0100

    Begin intergrating with user interface

--- public/engines/get-single-race.php
+++ public/engines/get-single-race.php
@@ -4,17 +4,23 @@ include_once 'required-functions.php';
 //Process race details
 $racesql = "SELECT `Regatta`, `R`, `D`, `Boat`, `Dist` FROM `races` WHERE `Key` = ?";
 $racesqlresultline = dbprepareandexecute($srrsdblink,$racesql,$raceid);
-$racesqlresultline = $racesqlresultline[0];
+if (count($racesqlresultline) > 0)
+  {
+  $racesqlresultline = $racesqlresultline[0];
+  }
+else
+  $racesqlresultline = "";
+
 include 'process-race-details.php';
 
 //Process paddlers
 $paddlersql = "SELECT `Position`, `Lane`, `Club`, `Crew`, `Time`, `NR`, `JSV`, `MW`, `CK` FROM `paddlers` WHERE `Race` = ?";
 $paddlerresults = dbprepareandexecute($srrsdblink,$paddlersql,$raceid);
 
-foreach ($paddlerresults as $paddlerkey=>$paddler)
+foreach ($paddlerresults as $paddlerkey=>$paddlerdetails)
   {
   include 'process-paddler-details.php';
-  $paddlerresults[$paddlerkey] = $paddler;
+  $paddlerresults[$paddlerkey] = $paddlerdetails;
   }
 
 $racedetails['Paddlers'] = $paddlerresults;
