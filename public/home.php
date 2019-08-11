49d8993812de8ea8e1fa7aa358453fc5f00ec4b1
Richard Crooks <email@rocrooks.co.uk>
2019-08-11 17:51:34 +0100

    Tidy up homepage

--- public/home.php
+++ public/home.php
@@ -2,15 +2,38 @@
 include 'defaulturls.php';
 
 //List of links
-$subsectionurls = array();
-$subsectionurls[0] = array("URL"=>$defaulturls['RegattasList'],"Text"=>"Browse Regattas");
-$subsectionurls[1] = array("URL"=>$defaulturls['ClubSearch'],"Text"=>"Club Search");
-$subsectionurls[2] = array("URL"=>$defaulturls['PaddlerSearch'],"Text"=>"Paddler Search");
-$subsectionurls[3] = array("URL"=>$defaulturls['EventRecords'],"Text"=>"Event Records");
-$subsectionurls[4] = array("URL"=>$defaulturls['Analytics'],"Text"=>"Regatta Analytics");
+$subsectionurls1 = array();
+$subsectionurls1[0] = array("URL"=>$defaulturls['RegattasList'],"Text"=>"Browse Regattas");
+$subsectionurls1[1] = array("URL"=>$defaulturls['ClubSearch'],"Text"=>"Club Search");
+$subsectionurls1[2] = array("URL"=>$defaulturls['PaddlerSearch'],"Text"=>"Paddler Search");
+$subsectionurls1[3] = array("URL"=>$defaulturls['EventRecords'],"Text"=>"Event Records");
+$subsectionurls1[4] = array("URL"=>$defaulturls['Analytics'],"Text"=>"Regatta Analytics");
 
-foreach($subsectionurls as $listurl)
+
+$subsectionurls2 = array();
+$subsectionurls2[0] = array("URL"=>"https://github.com/ROCrooks/sprintracingresultsservice","Text"=>"Git Repository","Target"=>"blank");
+
+echo '<div class="item">';
+echo '<p class="blockheading">Homepage</p>';
+echo '<p>Welcome to sprint racing results service, an online, searchable database of sprint canoe racing results from British national sprint regattas.<p>';
+echo '<p class="blockheading">Available Features</p>';
+echo '<p>Tools for searching the data in the sprint racing results service.<p>';
+foreach($subsectionurls1 as $listurl)
   {
-  echo '<p><a href="' . $listurl['URL'] . '">' . $listurl['Text'] . '</a></p>';
+  echo '<p><a href="' . $listurl['URL'] . '"';
+  if (isset($listurl['Target']) == true)
+    echo ' target="' . $listurl['Target'] . '"';
+  echo '>' . $listurl['Text'] . '</a></p>';
   }
+
+echo '<p class="blockheading">Support</p>';
+echo '<p>Technical information about the sprint racing results service.<p>';
+foreach($subsectionurls2 as $listurl)
+  {
+  echo '<p><a href="' . $listurl['URL'] . '"';
+  if (isset($listurl['Target']) == true)
+    echo ' target="' . $listurl['Target'] . '"';
+  echo '>' . $listurl['Text'] . '</a></p>';
+  }
+echo '</div>';
 ?>
