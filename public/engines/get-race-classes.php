d4361faca89482c730d4a96152abb0269217044e
Richard Crooks <email@rocrooks.co.uk>
2019-09-28 16:49:33 +0100

    Get the Key of a race class if asked for

--- public/engines/get-race-classes.php
+++ public/engines/get-race-classes.php
@@ -1,6 +1,11 @@
 <?php
 //Format the class of the race from the classes table
-$getclassdetailssql = "SELECT `JSV`, `MW`, `CK`, `Abil`, `Spec`, `Ages`, `FreeText` FROM `classes` WHERE `Race` = ? ";
+$getclassdetailssql = "SELECT ";
+if (isset($includeclassids) == true)
+  if ($includeclassids == true)
+    //Include class key if asked for
+    $getclassdetailssql = $getclassdetailssql . "`Key`, ";
+$getclassdetailssql = $getclassdetailssql . "`JSV`, `MW`, `CK`, `Abil`, `Spec`, `Ages`, `FreeText` FROM `classes` WHERE `Race` = ? ";
 
 //Prepare an SQL statement to run multiple times, as this engine can be called in a loop
 if (isset($retainedclassdetailssql) == false)
