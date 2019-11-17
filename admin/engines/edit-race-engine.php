0fd0851cf4d4c507eb2d16b311ed48682e619d62
Richard Crooks <email@rocrooks.co.uk>
2019-11-17 00:25:13 -0500

    Create the SQL and constraint arrays

--- admin/engines/edit-race-engine.php
+++ admin/engines/edit-race-engine.php
@@ -1,4 +1,6 @@
 <?php
+include_once 'required-functions.php';
+
 echo "<p>Form submitted</p>";
 
 //Get from the form
@@ -12,21 +14,107 @@ function getfromform($input)
   Return $output;
   }
 
-echo getfromform("JSV") . "<br>";
-echo getfromform("MW") . "<br>";
-echo getfromform("CK") . "<br>";
-echo getfromform("Regatta") . "<br>";
-echo getfromform("BoatSize") . "<br>";
-echo getfromform("Distance") . "<br>";
-echo getfromform("RoundDraw") . "<br>";
-echo getfromform("FreeText") . "<br>";
-echo getfromform("Abil") . "<br>";
-echo getfromform("Spec") . "<br>";
-echo getfromform("Ages") . "<br>";
-echo getfromform("Position") . "<br>";
-echo getfromform("Lane") . "<br>";
-echo getfromform("Crew") . "<br>";
-echo getfromform("Club") . "<br>";
-echo getfromform("Result") . "<br>";
-echo getfromform("PaddlerKey") . "<br>";
+//Get inputs from form
+$inputjsv = getfromform("JSV");
+$inputmw = getfromform("MW");
+$inputck = getfromform("CK");
+$inputregatta = getfromform("Regatta");
+$inputboatsize = getfromform("BoatSize");
+$inputdistance = getfromform("Distance");
+$inputrounddraw = getfromform("RoundDraw");
+$inputfreetext = getfromform("FreeText");
+$inputabil = getfromform("Abil");
+$inputspec = getfromform("Spec");
+$inputages = getfromform("Ages");
+$inputposition = getfromform("Position");
+$inputlane = getfromform("Lane");
+$inputcrew = getfromform("Crew");
+$inputclub = getfromform("Club");
+$inputresult = getfromform("Result");
+$inputkey = getfromform("ItemKey");
+
+
+
+//Convert input rounds into numbers
+if ($inputrounddraw != '')
+  {
+  $inputround = substr($inputrounddraw,0,1);
+
+  //Format the draw
+  $inputdraw = str_replace($inputround,"",$inputrounddraw);
+  if ($inputdraw == '')
+    $inputdraw = 0;
+  }
+else
+  {
+  $inputround = '';
+  $inputdraw = '';
+  }
+
+//Convert result to time and no result
+if (($inputresult == "DNS") OR ($inputresult == "DNF") OR ($inputresult == "DSQ") OR ($inputresult == "ERR") OR ($inputresult == "???"))
+  {
+  $inputnr = $inputresult;
+  $inputtime = 0;
+  }
+elseif ($inputresult != '')
+  {
+  $inputtime = hmstosecs($inputresult);
+  //Make NR blank if a valid time in seconds
+  if (is_numeric($inputtime) == true)
+    $inputnr = "";
+  else
+    {
+    $inputnr = "???";
+    $inputtime = 0;
+    }
+  }
+else
+  {
+  $inputnr = '???';
+  $inputtime = 0;
+  }
+
+//Make SQL and constraints
+if (isset($_POST['RaceEdit']) == true)
+  {
+  $runsql = "UPDATE `races` SET `Regatta` = ?, `Boat` = ?, `Dist` = ?, `R` = ?, `D` = ?, `FreeText` = ? WHERE `Key` = ?";
+  $runconstraints = array($inputregatta,$inputboatsize,$inputdistance,$inputround,$inputdraw,$inputfreetext,$raceid);
+  }
+if (isset($_POST['ClassEdit']) == true)
+  {
+  $runsql = "UPDATE `classes` SET `JSV` = ?, `MW` = ?, `CK` = ?, `Spec` = ?, `Abil` = ?, `Ages` = ?, `FreeText` = ? WHERE `Key` = ?";
+  $runconstraints = array($inputjsv,$inputmw,$inputck,$inputspec,$inputabil,$inputages,$inputfreetext,$inputkey);
+  }
+if (isset($_POST['ClassDelete']) == true)
+  {
+  $runsql = "DELETE FROM `classes` WHERE `Key` = ?";
+  $runconstraints = array($inputkey);
+  }
+if (isset($_POST['ClassAdd']) == true)
+  {
+  $runsql = "INSERT INTO `classes` (`JSV`, `MW`, `CK`, `Spec`, `Abil`, `Ages`, `FreeText`) VALUES (?, ?, ?, ?, ?, ?, ?)";
+  $runconstraints = array($inputjsv,$inputmw,$inputck,$inputspec,$inputabil,$inputages,$inputfreetext);
+  }
+if (isset($_POST['PaddlerEdit']) == true)
+  {
+  $runsql = "UPDATE `paddlers` SET `Position` = ?, `Lane` = ?, `Crew` = ?, `Club` = ?, `NR` = ?, `Time` = ?, `JSV` = ?, `MW` = ?, `CK` = ? WHERE `Key` = ?";
+  $runconstraints = array($inputposition,$inputlane,$inputcrew,$inputclub,$inputnr,$inputtime,$inputjsv,$inputmw,$inputck,$inputkey);
+  }
+if (isset($_POST['PaddlerDelete']) == true)
+  {
+  $runsql = "DELETE FROM `paddlers` WHERE `Key` = ?";
+  $runconstraints = array($inputkey);
+  }
+if (isset($_POST['PaddlerAdd']) == true)
+  {
+  $runsql = "INSERT INTO `paddlers` (`Position`, `Lane`, `Crew`, `Club`, `NR`, `Time`, `JSV`, `MW`, `CK`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  $runconstraints = array($inputposition,$inputlane,$inputcrew,$inputclub,$inputnr,$inputtime,$inputjsv,$inputmw,$inputck);
+  }
+
+if ((isset($runsql) == true) AND (isset($runconstraints) == true))
+  {
+  echo "<p>" . $runsql . "</p>";
+  echo "<p>" . implode(" - ",$runconstraints) . "</p>";
+  }
 ?>
