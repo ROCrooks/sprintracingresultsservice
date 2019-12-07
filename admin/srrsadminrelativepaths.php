8971d099d20d5f342ff8ae19c34090da10717726
Richard Crooks <email@rocrooks.co.uk>
2019-12-06 19:42:38 -0500

    Make relative paths directory point to absolute path

--- admin/srrsadminrelativepaths.php
+++ admin/srrsadminrelativepaths.php
@@ -1,5 +1,12 @@
 <?php
+
+//Get current directory
+//This makes the location independent of the location of the calling script
+$directory = dirname(__DIR__);
+$directory = explode("admin",$directory);
+$directory = $directory[0];
+
 //The relative path of all the public engines
-$publicenginesrelativepath = "../public/engines/";
-$adminenginesrelativepath = "engines/";
+$publicenginesrelativepath = $directory . "/public/engines/";
+$adminenginesrelativepath = $directory . "/admin/engines/";
 ?>
