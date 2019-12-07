9c3f43cb90e294de431c55ff14412f26d66d69db
Richard Crooks <email@rocrooks.co.uk>
2019-12-06 22:51:55 -0500

    Retrieve all classes as array using engine

---
+++ admin/class-manager.php
@@ -0,0 +1,30 @@
+<?php
+include 'srrsadminrelativepaths.php';
+
+include 'engines/class-getallclasses.php';
+
+print_r($uniqueclassnames);
+/*
+Assign classes and approve more
+
+List all classes - Delete Class
+
+Edit Autoclass
+
+
+
+Purge autoclass
+
+
+Engines Maybe Needed
+- Add class to races (class-addclassestoraces.php)
+- Add overall freetext to races (class-addfreetextstoraces.php)
+- Purge class (class-purgeclass.php)
+- Get each race name and corresponding autoclass (class-getallclasses.php)
+- Add autoclass (class-addautoclass.php)
+- Delete autoclass (class-deleteautoclass.php)
+- Add overall FreeText (class-addfreetext.php)
+- Delete overall FreeText (class-deletefreetext.php)
+- Update hidden race names (class-updateracename.php)
+*/
+?>
