ff052dcb24ffc90e26d54a39bc87725f9f55966e
Richard Crooks <email@rocrooks.co.uk>
2020-01-19 17:00:52 -0500

    Get all class details using engines

--- admin/edit-class.php
+++ admin/edit-class.php
@@ -7,13 +7,35 @@ $findclassname = $_GET['class'];
 
 //Get details about how this race is named in the records
 include $adminenginesrelativepath . "class-getoneclass.php";
-$autoclassdetails = $classdetails;
+$autoclassdetails = array();
+$autoclassdetails['Details'] = $classdetails;
+$autoclassdetails['ClassName'] = $autoclassname;
+//$autoclassdetails['Type'] = "Auto Class";
 
 //Get details about how this race is named in the records
 include $adminenginesrelativepath . "class-getraceclassnames.php";
+
+//Check if the autoclass needs adding
+if (in_array($autoclassdetails,$allraceclasses) === false)
+  {
+  array_push($allraceclasses,$autoclassdetails);
+  if (count($allraceclasses) > 1)
+    {
+    $autoclasswarning = "Warning, there is an autoclass specified which is
+    different to classes used in any of the races! Recommend purging this class
+    and re-assigning a single class to all races instead.";
+    }
+  }
+
+$racesformhtml = array();
+
+foreach($allraceclasses as $raceclass)
+  {
+
+  }
 ?>
 <div class="item">
 <p><?php echo $findclassname;?></p>
 
-<p><?php echo $autoclassname;?></p>
+<?php print_r($allraceclasses);?>
 </div>
