e11870e179d17c15906eb19811a0bb60a98e0906
Richard Crooks <email@rocrooks.co.uk>
2020-01-23 16:50:42 -0500

    Correct error in incrementing value

--- admin/engines/class-formtoclass.php
+++ admin/engines/class-formtoclass.php
@@ -3,10 +3,10 @@
 $fieldnames = array("Key","JSV","MW","CK","Abil","Spec","Ages","FreeText");
 
 //Number of the row
-$classrow = 1;
+$classrowkey = 1;
 
 //Name of the field to retrieve
-$checkfield = "FreeText" . $classrow;
+$checkfield = "FreeText" . $classrowkey;
 
 //Loop to read all rows
 $inputclassesarray = array();
@@ -19,7 +19,7 @@ while (isset($_POST[$checkfield]) == true)
   $classrow = array();
   foreach($fieldnames as $field)
     {
-    $formfield = $field . $classrow;
+    $formfield = $field . $classrowkey;
     $classrow[$field] = $_POST[$formfield];
 
     if ($classrow[$field] != '')
@@ -31,7 +31,7 @@ while (isset($_POST[$checkfield]) == true)
     array_push($inputclassesarray,$classrow);
 
   //Increment row and checkfield
-  $classrow++;
+  $classrowkey++;
   $checkfield = "FreeText" . $classrow;
   }
 
