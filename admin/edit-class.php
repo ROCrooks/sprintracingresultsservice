cc73152affb549dc635ea2e5766f378de955afa7
Richard Crooks <email@rocrooks.co.uk>
2020-01-25 18:00:35 -0500

    Class updates working and form correctly formatted

--- admin/edit-class.php
+++ admin/edit-class.php
@@ -5,12 +5,24 @@ include 'srrsadmindefaulturls.php';
 //Get class name from URL
 $findclassname = $_GET['class'];
 
+if (isset($_POST['AutoClass']) == true)
+  {
+  $autoclass = $_POST['AutoClass'];
+  if ($autoclass == "true")
+    $autoclass = true;
+  }
+else
+  $autoclass = false;
+
 //Update the
 if (isset($_POST['UpdateClass']) == true)
   {
   //Read the input form to get the class to add
   include $adminenginesrelativepath . "class-formtoclass.php";
   $classdetails = $inputclassesarray;
+
+  //Purge Old classes
+  include $adminenginesrelativepath . "class-purgeclasses.php";
 
   //Include the class to add
   include $adminenginesrelativepath . "class-assignclasses.php";
@@ -48,8 +60,14 @@ else
   //Get details about how this race is named in the records
   include $adminenginesrelativepath . "class-getraceclassnames.php";
 
+  //Check if there is an autoclass
+  if ($autoclassdetails['ClassName'] == "No Autoclass Specified")
+    {
+    $autoclasswarning = "Note: No autoclass is specified, if this is a common
+    class it is recommended to have one.";
+    }
   //Check if the autoclass needs adding
-  if (in_array($autoclassdetails,$allraceclasses) === false)
+  elseif (in_array($autoclassdetails,$allraceclasses) === false)
     {
     array_push($allraceclasses,$autoclassdetails);
     if (count($allraceclasses) > 1)
@@ -64,17 +82,37 @@ else
 //Array to store forms in
 $classesformhtml = array();
 
+//Set autoclass if there isn't one
+if (isset($autoclassdetails) == false)
+  $autoclassdetails = false;
+
 //Parameters needed for form generator
 $multirowform = true;
 $classformactionurl = $defaulturls['EditClass'] . $ahrefjoin . "class=" . $findclassname;
 //Make each form for race classes
 foreach($allraceclasses as $individualclass)
   {
+  //If it matches the autoclass set autoclass flag to true
+  if ($individualclass == $autoclassdetails)
+    $autoclass = true;
+
   //Make the form with the class list
   $classdetails = $individualclass['Details'];
   include $adminenginesrelativepath . "class-form-html.php";
 
-  //Wrap form in form
+  //Add the name of this class
+  $classformhtml = '<p>' . $individualclass['ClassName'] . '</p>' . $classformhtml;
+
+  //Add checkbox to make an autoclass
+  $classformhtml = $classformhtml . '<p>This is an autoclass: <input type="checkbox" name="AutoClass" value="checked"';
+
+  //Automatically checked if it is the class in the autoclass table
+  if ($autoclass == true)
+    $classformhtml = $classformhtml . ' checked';
+
+  $classformhtml = $classformhtml . '></p>';
+
+  //Wrap form in form HTML tags
   $classformhtml = $classformhtml . '<p><input type="submit" name="UpdateClass" value="Update"> <input type="submit" name="NewLine" value="New Line"></p>';
   $classformhtml = '<form action="' . $classformactionurl . '" method="post">' . $classformhtml . '</form>';
 
@@ -100,11 +138,10 @@ else
   $displaywarnings = '';
 ?>
 <div class="item">
-<p><?php echo $findclassname;?></p>
+<p>Editing the class: <?php echo $findclassname;?></p>
 
 <?php echo $displaywarnings; ?>
 
 <?php echo $classesformhtml; ?>
 
-<p>Test</p>
 </div>
