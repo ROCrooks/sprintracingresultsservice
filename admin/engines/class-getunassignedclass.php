66cabde2c94e5961226b4f7abdd8077e2d13a759
Richard Crooks <email@rocrooks.co.uk>
2020-01-25 18:12:12 -0500

    Engine for getting a single unset class name

---
+++ admin/engines/class-getunassignedclass.php
@@ -0,0 +1,14 @@
+<?php
+//Get required files
+include 'required-functions.php';
+
+//Get a single unset racename
+$getracenametosetsql = "SELECT `Class` FROM `races` WHERE `Set` = 8 ORDER BY `Class` ASC LIMIT 0,1";
+$racenametoset = dbprepareandexecute($srrsdblink,$getracenametosetsql);
+
+//Return the race name, or false if there is none
+if (count($racenametoset) == 1)
+  $racenametoset = $racenametoset[0]['Class'];
+else
+  $racenametoset = false;
+?>
