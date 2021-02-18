33029a90e1c9563ddb237e707998eb8c1e816530
Richard Crooks <email@rocrooks.co.uk>
2021-02-18 17:31:53 -0500

    Update list of default URLs for admin section

--- admin/engines/srrsadmindefaulturls.php
+++ admin/engines/srrsadmindefaulturls.php
@@ -1,13 +1,41 @@
 <?php
-$ahrefjoin = "?";
+//Get the current directory
+$currentdirectory = getcwd();
+$removedirs = array("/pages","/engines","/admin","/srrs");
+$currentdirectory = str_replace($removedirs,"",$currentdirectory);
+
+if ((strpos($currentdirectory,"rocrooks.ca") === false) AND (strpos($currentdirectory,"live") === false) AND (strpos($currentdirectory,"newcms") === false))
+  {
+  $ahrefjoin = "?";
+  $urlcell = "URL";
+  }
+else
+  {
+  $ahrefjoin = "&";
+  $urlcell = "PageName";
+  }
+
+//Format URL variables
+if (isset($urlvariables) == true)
+  $urlvariables = "?" . implode("&",$urlvariables);
 
 //Config file with default URLs for SRRS
 $defaulturls = array();
-$defaulturls['AddRegatta'] = "add-regatta.php";
-$defaulturls['EditRegatta'] = "edit-regatta.php";
-$defaulturls['EditRace'] = "edit-race.php";
-$defaulturls['ManageRegattas'] = "manage-regattas.php";
-$defaulturls['ManageClasses'] = "class-manager.php";
-$defaulturls['EditClass'] = "edit-class.php";
-$defaulturls['AddClass'] = "add-class.php";
+$defaulturls['AddRegatta'] = array("URL"=>"add-regatta.php","PageName"=>"AddRegatta");
+$defaulturls['EditRegatta'] = array("URL"=>"edit-regatta.php","PageName"=>"EditRegatta");
+$defaulturls['EditRace'] = array("URL"=>"edit-race.php","PageName"=>"EditRace");
+$defaulturls['ManageRegattas'] = array("URL"=>"manage-regattas.php","PageName"=>"ManageRegattas");
+$defaulturls['ManageClasses'] = array("URL"=>"class-manager.php","PageName"=>"ManageClasses");
+$defaulturls['EditClass'] = array("URL"=>"edit-class.php","PageName"=>"EditClass");
+$defaulturls['AddClass'] = array("URL"=>"add-class.php","PageName"=>"AddClass");
+
+//Get the appropriate URL or page name
+foreach ($defaulturls as $defaultkey=>$options)
+  {
+  //Put the ?page= onto the URL
+  if ($urlcell == "PageName")
+    $options[$urlcell] = "?page=" . $options[$urlcell];
+
+  $defaulturls[$defaultkey] = $options[$urlcell];
+  }
 ?>
