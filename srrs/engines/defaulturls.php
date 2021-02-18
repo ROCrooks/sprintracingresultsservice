9d71db23b04c0870ce3390b2cc06990181b34a2d
Richard Crooks <email@rocrooks.co.uk>
2021-02-17 21:39:15 -0500

    Add default URLs for when files are included in CMS

--- srrs/engines/defaulturls.php
+++ srrs/engines/defaulturls.php
@@ -20,22 +20,29 @@ if (isset($urlvariables) == true)
   $urlvariables = "?" . implode("&",$urlvariables);
 
 //The URLs and Page Names
-$defaulturls['Analytics'] = array("URL"=>"analytics.php","PageName"=>"");
-$defaulturls['EventRecords'] = array("URL"=>"event-records.php","PageName"=>"");
-$defaulturls['ClubSearch'] = array("URL"=>"club-search.php","PageName"=>"");
-$defaulturls['PaddlerSearch'] = array("URL"=>"paddler-search.php","PageName"=>"");
-$defaulturls['RegattasList'] = array("URL"=>"regattalist.php","PageName"=>"");
-$defaulturls['RegattaLookup'] = array("URL"=>"regatta-results-lookup.php","PageName"=>"");
-$defaulturls['RegattaResults'] = array("URL"=>"regatta-results.php","PageName"=>"");
-$defaulturls['RaceResults'] = array("URL"=>"race-display.php","PageName"=>"");
-$defaulturls['PaddlerPage'] = array("URL"=>"paddler-page.php","PageName"=>"");
-$defaulturls['ClubPage'] = array("URL"=>"club-page.php","PageName"=>"");
+$defaulturls['Analytics'] = array("URL"=>"analytics.php","PageName"=>"RegattaAnalytics");
+$defaulturls['EventRecords'] = array("URL"=>"event-records.php","PageName"=>"EventRecords");
+$defaulturls['ClubSearch'] = array("URL"=>"club-search.php","PageName"=>"ClubSearch");
+$defaulturls['PaddlerSearch'] = array("URL"=>"paddler-search.php","PageName"=>"PaddlerSearch");
+$defaulturls['RegattasList'] = array("URL"=>"regattalist.php","PageName"=>"RegattaList");
+$defaulturls['RegattaLookup'] = array("URL"=>"regatta-results-lookup.php","PageName"=>"ResultsLookup");
+$defaulturls['RegattaResults'] = array("URL"=>"regatta-results.php","PageName"=>"RegattaResults");
+$defaulturls['RaceResults'] = array("URL"=>"race-display.php","PageName"=>"RaceView");
+$defaulturls['PaddlerPage'] = array("URL"=>"paddler-page.php","PageName"=>"PaddlerPage");
+$defaulturls['ClubPage'] = array("URL"=>"club-page.php","PageName"=>"ClubHome");
 $defaulturls['TopNTimes'] = array("URL"=>"toptimes.php","PageName"=>"");
 $defaulturls['TopNTimesBrowser'] = array("URL"=>"eventbesttimes.php","PageName"=>"");
+$defaulturls['RestInstructions'] = array("URL"=>"","PageName"=>"RESTInfo");
+$defaulturls['TimesAnalysis'] = array("URL"=>"","PageName"=>"");
+$defaulturls['TimesAnalysisBrowser'] = array("URL"=>"","PageName"=>"");
 
 //Get the appropriate URL or page name
 foreach ($defaulturls as $defaultkey=>$options)
   {
+  //Put the ?page= onto the URL
+  if ($urlcell == "PageName")
+    $options[$urlcell] = "?page=" . $options[$urlcell];
+
   $defaulturls[$defaultkey] = $options[$urlcell];
   }
 ?>
