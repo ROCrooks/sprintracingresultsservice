e6050da4e54399b52b755239ab92663062ea14b8
Richard Crooks <email@rocrooks.co.uk>
2021-02-22 21:36:26 -0500

    Move the links table to a separate engine to be shared

---
+++ srrs/engines/create-event-links.php
@@ -0,0 +1,117 @@
+<?php
+//Required functions
+include 'required-functions.php';
+
+//Container for the base SQL constraints
+$sqlcheckbaseconstraints = array();
+
+//Create the SQL to check if the
+$sqlcheckforevent = "SELECT COUNT(*) FROM `paddlers` p
+LEFT JOIN `races` r ON r.`Key` = p.`Race` WHERE ";
+
+//Attach the club constraint to the URL and SQL
+if (isset($club) == true)
+  {
+  if ($club != '')
+    {
+    //Add club to URL
+    $lookupurl = $lookupurl . $ahrefjoin . "club=" . $club;
+    $ahrefjoin = "&";
+
+    //Add club wildcard to constraint
+    $clubwildcard = "*" . $club . "*";
+    array_push($sqlcheckconstraints);
+
+    $sqlcheckforevent = $sqlcheckforevent . " p.`Club` LIKE ? AND ";
+    }
+  }
+
+$sqlcheckforevent = $sqlcheckforevent . "p.`MW` = ? AND p.`CK` = ? AND r.`Boat` = ? AND r.`Dist` = ?";
+
+//Loop through MK, WK, MC, WC
+$searches = array();
+$searches[0] = array("MW"=>"M","CK"=>"K","Boat"=>1);
+$searches[1] = array("MW"=>"M","CK"=>"K","Boat"=>2);
+$searches[2] = array("MW"=>"M","CK"=>"K","Boat"=>4);
+$searches[3] = array("MW"=>"W","CK"=>"K","Boat"=>1);
+$searches[4] = array("MW"=>"W","CK"=>"K","Boat"=>2);
+$searches[5] = array("MW"=>"W","CK"=>"K","Boat"=>4);
+$searches[6] = array("MW"=>"M","CK"=>"C","Boat"=>1);
+$searches[7] = array("MW"=>"M","CK"=>"C","Boat"=>2);
+$searches[8] = array("MW"=>"M","CK"=>"C","Boat"=>4);
+$searches[9] = array("MW"=>"W","CK"=>"C","Boat"=>1);
+$searches[10] = array("MW"=>"W","CK"=>"C","Boat"=>2);
+$searches[11] = array("MW"=>"W","CK"=>"C","Boat"=>4);
+
+//Distances and tops to loop
+$distances = array(200,500,1000,5000);
+$jsvs = array("","J","V");
+
+//Column widths for the
+$columnwidths = array();
+$columnwidths['Name'] = 100;
+$columnwidths['Distance'] = 100;
+$totalwidth = $columnwidths['Name']+($columnwidths['Distance']*4);
+
+//Make the HTML for the output table
+$eventslisthtml = '<div style="display: table; margin: auto; width: ' . $totalwidth . 'px;">';
+
+//Make the HTML for the output table
+$eventslisthtml = $eventslisthtml . '<div style="display: table-row; margin: auto;">';
+$eventslisthtml = $eventslisthtml . '</div>';
+
+foreach ($searches as $search)
+  {
+  //The standard URL constraints
+  $standardurlconstraints = "mw=" . $search['MW'] . "&ck=" . $search['CK'] . "&boat=" . $search['Boat'] . "&find=10";
+
+  //Make name of the event class
+  if ($search['MW'] == "M")
+    $name = "Mens";
+  if ($search['MW'] == "W")
+    $name = "Womens";
+
+  $name = $name . " " . $search['CK'] . $search['Boat'];
+
+  $eventslisthtml = $eventslisthtml . '<div style="display: table-row; margin: auto;">';
+  $eventslisthtml = $eventslisthtml . '<div style="display: table-cell; width: ' . $columnwidths['Name'] . 'px;"><p>' . $name . '</p></div>';
+
+  //Loop each distance for finding top times
+  foreach($distances as $distance)
+    {
+    $cellhtml = '<p>' . $distance . 'm</p>';
+
+    //Attach the distance to the URL
+    $distanceurl = $standardurlconstraints . "&distance=" . $distance;
+
+    //Loop each top N times list and put them below the distance
+    $jsvshtml = array();
+    foreach ($jsvs as $jsv)
+      {
+      $fullurl = $lookupurl . $ahrefjoin . $distanceurl;
+      //Add the search
+      if ($jsv != "")
+        $fullurl = $fullurl . "&jsv=" . $jsv;
+
+      if ($jsv == "")
+        $linkname = "Overall";
+      if ($jsv == "J")
+        $linkname = "Junior";
+      if ($jsv == "V")
+        $linkname = "Masters";
+
+      //Make the HTML link for finding the top N results
+      $jsvhtml = '<a href="' . $fullurl . '">' . $linkname . '</a>';
+      array_push($jsvshtml,$jsvhtml);
+      }
+    $jsvshtml = '<p>' . implode("<br>",$jsvshtml) . '</p>';
+    $cellhtml = $cellhtml . $jsvshtml;
+
+    $eventslisthtml = $eventslisthtml . '<div style="display: table-cell; width: ' . $columnwidths['Distance'] . 'px;">' . $cellhtml . '</div>';
+    }
+
+  $eventslisthtml = $eventslisthtml . '</div>';
+  }
+
+$eventslisthtml = $eventslisthtml . '</div>';
+?>
--- srrs/pages/eventbesttimes.php
+++ srrs/pages/eventbesttimes.php
@@ -13,83 +13,11 @@ if (strpos($defaulturls['RegattaLookup'],"?") === false)
 else
   $ahrefjoin = "&";
 
-//Loop through MK, WK, MC, WC
-$searches = array();
-$searches[0] = array("MW"=>"M","CK"=>"K","Boat"=>1);
-$searches[1] = array("MW"=>"M","CK"=>"K","Boat"=>2);
-$searches[2] = array("MW"=>"M","CK"=>"K","Boat"=>4);
-$searches[3] = array("MW"=>"W","CK"=>"K","Boat"=>1);
-$searches[4] = array("MW"=>"W","CK"=>"K","Boat"=>2);
-$searches[5] = array("MW"=>"W","CK"=>"K","Boat"=>4);
-$searches[6] = array("MW"=>"M","CK"=>"C","Boat"=>1);
-$searches[7] = array("MW"=>"M","CK"=>"C","Boat"=>2);
-$searches[8] = array("MW"=>"M","CK"=>"C","Boat"=>4);
-$searches[9] = array("MW"=>"W","CK"=>"C","Boat"=>1);
-$searches[10] = array("MW"=>"W","CK"=>"C","Boat"=>2);
-$searches[11] = array("MW"=>"W","CK"=>"C","Boat"=>4);
+//Get URL to send to the engine
+$lookupurl = $defaulturls['TopNTimes'];
 
-//Distances and tops to loop
-$distances = array(200,500,1000,5000);
-$tops = array(10,20,50);
-
-//Column widths for the
-$columnwidths = array();
-$columnwidths['Name'] = 100;
-$columnwidths['Distance'] = 100;
-$totalwidth = $columnwidths['Name']+($columnwidths['Distance']*4);
-
-//Make the HTML for the output table
-$eventslisthtml = '<div style="display: table; margin: auto; width: ' . $totalwidth . 'px;">';
-
-//Make the HTML for the output table
-$eventslisthtml = $eventslisthtml . '<div style="display: table-row; margin: auto;">';
-$eventslisthtml = $eventslisthtml . '<div style="display: table-cell; width: ' . $columnwidths['Name'] . 'px; cellspan: 5;"><p>Overall</p></div>';
-$eventslisthtml = $eventslisthtml . '</div>';
-
-foreach ($searches as $search)
-  {
-  //The standard URL constraints
-  $standardurlconstraints = "mw=" . $search['MW'] . "&ck=" . $search['CK'] . "&boat=" . $search['Boat'];
-
-  //Make name of the event class
-  if ($search['MW'] == "M")
-    $name = "Mens";
-  if ($search['MW'] == "W")
-    $name = "Womens";
-
-  $name = $name . " " . $search['CK'] . $search['Boat'];
-
-  $eventslisthtml = $eventslisthtml . '<div style="display: table-row; margin: auto;">';
-  $eventslisthtml = $eventslisthtml . '<div style="display: table-cell; width: ' . $columnwidths['Name'] . 'px;"><p>' . $name . '</p></div>';
-
-  //Loop each distance for finding top times
-  foreach($distances as $distance)
-    {
-    $cellhtml = '<p>' . $distance . 'm</p>';
-
-    //Attach the distance to the URL
-    $distanceurl = $standardurlconstraints . "&distance=" . $distance;
-
-    //Loop each top N times list and put them below the distance
-    $topshtml = array();
-    foreach ($tops as $top)
-      {
-      $fullurl = $defaulturls['TopNTimes'] . $ahrefjoin . $distanceurl . "&find=" . $top;
-      $top = "Top " . $top;
-      //Make the HTML link for finding the top N results
-      $tophtml = '<a href="' . $fullurl . '">' . $top . '</a>';
-      array_push($topshtml,$tophtml);
-      }
-    $topshtml = '<p>' . implode("<br>",$topshtml) . '</p>';
-    $cellhtml = $cellhtml . $topshtml;
-
-    $eventslisthtml = $eventslisthtml . '<div style="display: table-cell; width: ' . $columnwidths['Distance'] . 'px;">' . $cellhtml . '</div>';
-    }
-
-  $eventslisthtml = $eventslisthtml . '</div>';
-  }
-
-$eventslisthtml = $eventslisthtml . '</div>';
+//Create the event links page
+include $enginesdirectory . 'create-event-links.php';
 ?>
 
 <?php echo $eventslisthtml; ?>
