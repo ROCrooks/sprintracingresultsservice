84e6d8371cb32fc69820bf979b89578790bdd339
Richard Crooks <email@rocrooks.co.uk>
2021-02-24 21:27:23 -0500

    Add description of the event best times page

--- srrs/pages/eventbesttimes.php
+++ srrs/pages/eventbesttimes.php
@@ -19,5 +19,9 @@ $lookupurl = $defaulturls['TopNTimes'];
 //Create the event links page
 include $enginesdirectory . 'create-event-links.php';
 ?>
-
+<div class="item">
+<p class="blockheading">Find Best Paddlers</p>
+<p>Find a list of the best paddlers, ranked by fastest PB in each event at all
+  regattas. Results are available for overall, as well as junior and masters events<p>
 <?php echo $eventslisthtml; ?>
+</div>
