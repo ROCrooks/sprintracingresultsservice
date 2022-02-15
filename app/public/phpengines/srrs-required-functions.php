78ec17191159909dee96747df8fd15d50ad49600
Richard Crooks <email@rocrooks.co.uk>
2022-02-14 20:41:04 -0500

    Rename required functions file to make is specific to SRRS

--- app/public/phpengines/srrs-required-functions.php
+++ app/public/phpengines/srrs-required-functions.php
@@ -1,10 +1,26 @@
 <?php
-//Get the directory of the engines
+//Get the functions directory
 $currentdirectory = getcwd();
 $removedirs = array("/pages","/engines","/admin","/srrs");
 $currentdirectory = str_replace($removedirs,"",$currentdirectory);
-$enginesdirectory = $currentdirectory . "/srrs/engines/";
+$admindirectory = $currentdirectory . "/admin/";
+$functionsdirectory = $currentdirectory . "/functions/";
 
-//Include the same files as for the main SRRS
-include $enginesdirectory . "required-functions.php";
+//Get the required function files
+$files = array("srrs","userinput","db","array","drawgraph","document");
+
+//Include each required functions file
+foreach ($files as $file)
+  {
+  $url = $functionsdirectory . "/" . $file . "-functions.php";
+  include_once $url;
+  }
+
+//Create database connection
+//Only create if not already created
+if (isset($srrsdblink) == false)
+  {
+  include_once $admindirectory . '/dbconnect-function.php';
+  $srrsdblink = createdbconnection("SRRS");
+  }
 ?>
