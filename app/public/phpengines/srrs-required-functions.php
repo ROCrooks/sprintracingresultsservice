54caba99c76d1ee34d8a2a014340470adc6dc263
Richard Crooks <email@rocrooks.co.uk>
2022-02-14 20:45:46 -0500

    Get the function files location from the index page instead

--- app/public/phpengines/srrs-required-functions.php
+++ app/public/phpengines/srrs-required-functions.php
@@ -1,18 +1,11 @@
 <?php
-//Get the functions directory
-$currentdirectory = getcwd();
-$removedirs = array("/pages","/engines","/admin","/srrs");
-$currentdirectory = str_replace($removedirs,"",$currentdirectory);
-$admindirectory = $currentdirectory . "/admin/";
-$functionsdirectory = $currentdirectory . "/functions/";
-
 //Get the required function files
 $files = array("srrs","userinput","db","array","drawgraph","document");
 
 //Include each required functions file
 foreach ($files as $file)
   {
-  $url = $functionsdirectory . "/" . $file . "-functions.php";
+  $url = $functionslocation . "/" . $file . "-functions.php";
   include_once $url;
   }
 
@@ -20,7 +13,7 @@ foreach ($files as $file)
 //Only create if not already created
 if (isset($srrsdblink) == false)
   {
-  include_once $admindirectory . '/dbconnect-function.php';
+  include_once $functionslocation . '/dbconnect-function.php';
   $srrsdblink = createdbconnection("SRRS");
   }
 ?>
