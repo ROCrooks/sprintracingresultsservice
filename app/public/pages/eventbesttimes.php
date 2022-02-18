b47f7f32581591060e973d23a9e76200d8da379f
Richard Crooks <email@rocrooks.co.uk>
2022-02-18 17:53:38 -0500

    Wrap output as section

--- app/public/pages/eventbesttimes.php
+++ app/public/pages/eventbesttimes.php
@@ -2,10 +2,10 @@
 //Create the event links page
 include $engineslocation . 'create-event-links.php';
 
-$pagehtml = '<div class="item">
+$pagehtml = '<section>
 <p class="blockheading">Find Best Paddlers</p>
 <p>Find a list of the best paddlers, ranked by fastest PB in each event at all
   regattas. Results are available for overall, as well as junior and masters events<p>';
 $pagehtml = $pagehtml . $eventslisthtml;
-$pagehtml = $pagehtml . '</div>';
+$pagehtml = $pagehtml . '</section>';
 ?>
