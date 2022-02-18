0d3a0025258fd517b0987c537cb308012d1524ae
Richard Crooks <email@rocrooks.co.uk>
2022-02-18 17:16:28 -0500

    Update the event best times page for the new CMS

--- app/public/pages/eventbesttimes.php
+++ app/public/pages/eventbesttimes.php
@@ -1,27 +1,14 @@
 <?php
-//Get the directory of the engines
-$currentdirectory = getcwd();
-$removedirs = array("/pages","/engines","/admin","/srrs");
-$currentdirectory = str_replace($removedirs,"",$currentdirectory);
-$enginesdirectory = $currentdirectory . "/srrs/engines/";
-
-include $enginesdirectory . 'defaulturls.php';
-
-//Define join to attach URL variables
-if (strpos($defaulturls['RegattaLookup'],"?") === false)
-  $ahrefjoin = "?";
-else
-  $ahrefjoin = "&";
-
 //Get URL to send to the engine
-$lookupurl = $defaulturls['TopNTimes'];
+$lookupurl = "TopPerformances";
 
 //Create the event links page
-include $enginesdirectory . 'create-event-links.php';
-?>
-<div class="item">
+include $engineslocation . 'create-event-links.php';
+
+$pagehtml = '<div class="item">
 <p class="blockheading">Find Best Paddlers</p>
 <p>Find a list of the best paddlers, ranked by fastest PB in each event at all
-  regattas. Results are available for overall, as well as junior and masters events<p>
-<?php echo $eventslisthtml; ?>
-</div>
+  regattas. Results are available for overall, as well as junior and masters events<p>';
+$pagehtml = $pagehtml . $eventslisthtml;
+$pagehtml = $pagehtml . '</div>';
+?>
