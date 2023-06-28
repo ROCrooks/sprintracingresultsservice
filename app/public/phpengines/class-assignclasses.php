73d3d0fc4d2491b345053f5288abcc073b47dfe7
Richard Crooks <email@rocrooks.co.uk>
2023-06-28 19:38:51 -0400

    The class-assignclasses.php engine can now deal with atomized race names and insert these into the autoclasses individually

--- app/public/phpengines/class-assignclasses.php
+++ app/public/phpengines/class-assignclasses.php
@@ -1,6 +1,7 @@
 <?php
 include_once $engineslocation . 'srrs-required-functions.php';
 
+//Make the add classes statement
 if (isset($insertclassstmt) == false)
   {
   //Create insert class statement if not already created
@@ -13,7 +14,7 @@ $getracessql = "SELECT `Key` FROM `races` WHERE `Class` = ?";
 $racekeys = dbprepareandexecute($srrsdblink,$getracessql,$findclassname);
 $racekeys = resulttocolumn($racekeys);
 
-//For each race key
+//For each race key and the list of classes
 foreach ($racekeys as $racekey)
   {
   //For each class from the class details
@@ -32,26 +33,23 @@ if (isset($setracesclassesstmt) == false)
   $setracesclassesstmt = dbprepare($srrsdblink,$setracesclassessql);
   }
 
+//Update the race to say that the class has been set
 dbexecute($setracesclassesstmt,$findclassname);
 
-//Add the autoclass if it requested
-if (isset($autoclass) == false)
-  $autoclass = false;
-
-if ($autoclass == true)
+//Check each atomized class to see if it is to be added as an AutoClass for future use
+foreach ($classdetails as $classline)
   {
-  //Prepare statement to add the autoclass
-  if (isset($addautoclassstmt) == false)
+  if ($classline['AutoClass'] == false)
     {
-    $addautoclasssql = "INSERT INTO `autoclasses` (`RaceName`, `JSV`, `MW`, `CK`, `Spec`, `Abil`, `Ages`, `FreeText`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-    $addautoclassstmt = dbprepare($srrsdblink,$addautoclasssql);
-    }
+    //Prepare statement to add the autoclass
+    if (isset($addautoclassstmt) == false)
+      {
+      $addautoclasssql = "INSERT INTO `autoclasses` (`RaceName`, `JSV`, `MW`, `CK`, `Spec`, `Abil`, `Ages`, `FreeText`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+      $addautoclassstmt = dbprepare($srrsdblink,$addautoclasssql);
+      }
 
-  //Add each class as an autoclass
-  foreach ($classdetails as $classline)
-    {
     //Add class to the autoclasses table
-    $classaddconstraints = array($findclassname,$classline['JSV'],$classline['MW'],$classline['CK'],$classline['Spec'],$classline['Abil'],$classline['Ages'],$classline['FreeText']);
+    $classaddconstraints = array($classline['RaceName'],$classline['JSV'],$classline['MW'],$classline['CK'],$classline['Spec'],$classline['Abil'],$classline['Ages'],$classline['FreeText']);
     dbexecute($addautoclassstmt,$classaddconstraints);
     }
   }
