52ae615ca22b82f7985cb7a0aaf78719bbafd70e
Richard Crooks <email@rocrooks.co.uk>
2023-07-02 20:37:46 -0400

    Remove searching for the atomized autoclasses from the database and put into a separate engine so that it can be used in the set classes page

--- app/public/phpengines/import-race-db.php
+++ app/public/phpengines/import-race-db.php
@@ -40,40 +40,9 @@ foreach ($allpaddlerdetails as $paddlerdetails)
   dbexecute($paddlerstmt,$insertpaddlerdetails);
   }
 
-//Separate RaceName into component parts - components are stored as separate auto races
-//Standardize all breaks into "+"
-$variablebreaks = array(" +"," + ","+ "," &"," & ","& ","&")
-$racenamecomponents = str_replace($variablebreaks,"+",$racedetails['RaceName']);
-
-//Find out if it's a kayak class or a canoe class
-$racenamecomponents = explode(" ",$racenamecomponents);
-$boattype = array_pop($racenamecomponents);
-
-//If the boat type is only 1 letter, add it to each race class
-if(strlen($boattype) == 1)
-  {
-  foreach ($racenamecomponents as $racenamekey=>$racename)
-    {
-    $racenamecomponents[$racenamekey] = $racename . " " . $boattype;
-    }
-  }
-
-//Find matching race classes in the autoclasses list
-$foundautoclasses = array();
-foreach($racenamecomponents as $namecomponent)
-  {
-  //Find the AutoClasses
-  if (isset($findclassstmt) == false)
-    {
-    $findclasssql = "SELECT * FROM `autoclasses` WHERE `RaceName` = ?";
-  	$findclassstmt = dbprepare($srrsdblink,$findclasssql);
-    }
-
-  //Add the autoclass if it's found
-  $autoclass = dbexecute($findclassstmt,$namecomponent);
-  if (count($autoclass) > 0)
-    array_push($foundautoclasses,$autoclass);
-  }
+//Look for any currently set autoclasses
+$racenameinput = $racedetails['RaceName']
+include $engineslocation . "find-autoclasses.php";
 
 if (count($foundautoclasses) == count($racenamecomponents))
   {
