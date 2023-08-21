fc22b939b7dc8273f79a006117f755fec4d46538
Richard Crooks <richard@rocrooks.ca>
2023-08-20 21:30:40 -0400

    Remove debugging code from race import script

--- app/public/phpengines/import-race-db.php
+++ app/public/phpengines/import-race-db.php
@@ -44,21 +44,11 @@ foreach ($allpaddlerdetails as $paddlerdetails)
 $findclassname = $racedetails['RaceName'];
 include $engineslocation . "find-autoclasses.php";
 
-echo "Found Auto classes<br>";
-print_r($foundautoclasses);
-echo "<br>";
-echo "The race name components<br>";
-print_r($racenamecomponents);
-echo "<br>";
-
 if (count($foundautoclasses) == count($racenamecomponents))
   {
   //Add each race class
   foreach ($foundautoclasses as $classadd)
     {
-    echo "Add Class Array<br>";
-    print_r($classadd);
-    echo "<br>";
     if (isset($insertclassstmt) == false)
       {
       //Create insert class statement if not already created
