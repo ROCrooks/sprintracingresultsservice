f103db1914b73edafb6c0c9524b477fb3f162ef2
Richard Crooks <richard@rocrooks.ca>
2023-11-17 08:04:31 -0500

    Remove functionality that adds classes when races are imported - may re-add in future

--- app/public/phpengines/import-race-db.php
+++ app/public/phpengines/import-race-db.php
@@ -41,42 +41,5 @@ foreach ($allpaddlerdetails as $paddlerdetails)
   }
 
 //Look for any currently set autoclasses
-$findclassname = $racedetails['RaceName'];
-include $engineslocation . "find-autoclasses.php";
-
-//Get the distinct classes found as autoclasses
-$distinctclasses = array();
-foreach($foundautoclasses as $foundautoclass)
-  {
-  if(in_array($foundautoclass['RaceName'],$distinctclasses) == false)
-    array_push($distinctclasses,$foundautoclass['RaceName']);
-  }
-
-if (count($distinctclasses) == count($racenamecomponents))
-  {
-  //Add each race class
-  foreach ($foundautoclasses as $classadd)
-    {
-    if (isset($insertclassstmt) == false)
-      {
-      //Create insert class statement if not already created
-      $insertclasssql = "INSERT INTO `classes` (`Race`, `JSV`, `MW`, `CK`, `Spec`, `Abil`, `Ages`, `FreeText`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-    	$insertclassstmt = dbprepare($srrsdblink,$insertclasssql);
-      }
-
-    $insertclassdetails = array($raceid,$classadd['JSV'],$classadd['MW'],$classadd['CK'],$classadd['Spec'],$classadd['Abil'],$classadd['Ages'],$classadd['FreeText']);
-    dbexecute($insertclassstmt,$insertclassdetails);
-    }
-
-  //Prepare the set race query if not already prepared
-  if (isset($setracestmt) == false)
-    {
-    $setracesql = "UPDATE `races` SET `Set` = 1 WHERE `Key` = ?";
-    $setracestmt = dbprepare($srrsdblink,$setracesql);
-    }
-
-  //Execute set race query as the class has been added by the race import script
-  dbexecute($setracestmt,$raceid);
-  }
-
+//$findclassname = $racedetails['RaceName'];
 ?>
