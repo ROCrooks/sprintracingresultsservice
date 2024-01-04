98af4e3f4c2615cada73c03a9e7208396cd402f0
Richard Crooks <richard@rocrooks.ca>
2024-01-04 16:16:50 -0500

    Retrieve the class band from the race classes

--- app/public/phpengines/get-race-classes.php
+++ app/public/phpengines/get-race-classes.php
@@ -5,7 +5,7 @@ if (isset($includeclassids) == true)
   if ($includeclassids == true)
     //Include class key if asked for
     $getclassdetailssql = $getclassdetailssql . "`Key`, ";
-$getclassdetailssql = $getclassdetailssql . "`JSV`, `MW`, `CK`, `Abil`, `Spec`, `Ages`, `FreeText` FROM `classes` WHERE `Race` = ? ";
+$getclassdetailssql = $getclassdetailssql . "`JSV`, `MW`, `CK`, `Abil`, `Spec`, `Ages`, `Band`, `FreeText` FROM `classes` WHERE `Race` = ? ";
 
 //Prepare an SQL statement to run multiple times, as this engine can be called in a loop
 if (isset($retainedclassdetailssql) == false)
