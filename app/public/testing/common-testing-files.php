42b1f74fb69e3a107b53ad1e306ecba0bf26fcf1
Richard Crooks <richard@rocrooks.ca>
2024-04-19 22:53:18 -0400

    Create testing engine

---
+++ app/public/testing/common-testing-files.php
@@ -0,0 +1,10 @@
+<?php
+//Define locations for files
+$rootdirectory = '/app/public/';
+$functionslocation = $rootdirectory . 'phplibs/';
+$engineslocation = $rootdirectory . 'phpengines/';
+$jsonslocation = $rootdirectory . 'meta/';
+$pageslocation = $rootdirectory . 'pages/';
+$cmslocation = $rootdirectory . 'cms/';
+$tempfileslocation = $rootdirectory . 'tempfiles/';
+?>
---
+++ app/public/testing/process-paddler-input.php
@@ -0,0 +1,3 @@
+<?php
+include 'common-testing-files.php';
+?>
