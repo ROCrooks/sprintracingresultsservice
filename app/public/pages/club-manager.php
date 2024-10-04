46e2c7fec7ec6a815f8ce00b42f6bcabe18443ae
Richard Crooks <richard@rocrooks.ca>
2024-10-04 18:44:02 -0400

    Retrieve the clubs from a different engine

--- app/public/pages/club-manager.php
+++ app/public/pages/club-manager.php
@@ -1,7 +1,12 @@
 <?php
 include_once $engineslocation . 'srrs-required-functions.php';
 
-include $engineslocation . 'club-manager-engine.php';
+include $engineslocation . 'club-list-engine.php';
+
+print_r($clubdetailsresult);
+echo "<br>";
+print_r($orphanclubs);
+echo "<br>";
 
 $pagehtml = "<section><p>Test</p></section>";
 ?>
