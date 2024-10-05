f283c497a7c5b243e09b821aa7e0ca076485990d
Richard Crooks <richard@rocrooks.ca>
2024-10-04 21:44:12 -0400

    Find club colours that are missing clubs from the database

--- app/public/phpengines/club-list-engine.php
+++ app/public/phpengines/club-list-engine.php
@@ -39,7 +39,42 @@ foreach ($paddlerclubs as $paddlerclub)
         }
     }
 
+//Make a list of club colour files
+$clubcolourfiles = array();
+if (is_dir($clubcoloursfileslocation))
+    {
+    if ($dh = opendir($clubcoloursfileslocation))
+        {
+        while (($file = readdir($dh)) !== false)
+            {
+            $filetype = filetype($clubcoloursfileslocation . $file);
+            $filename = $file;
+
+            if (($filetype == 'file') AND ($file != "unknown.png"))
+                {
+                $file = substr($file,0,3);
+                array_push($clubcolourfiles,$file);
+                }
+            }
+        closedir($dh);
+        }
+    }
+
+//Orphan colours array
+$orphancolours = array();
+
+//Add found clubs to the database clubs array if they're not already present
+foreach ($clubcolourfiles as $clubcolour)
+    {
+    //Check to see if the club code is missing from the array, and if not, add to orphan club array
+    if ((in_array($clubcolour,$allclubscodeslist) === false) AND ($clubcolour != ''))
+        {
+        array_push($orphancolours,$clubcolour);
+        }
+    }
+
 //Unset unneeded arrays
+unset($clubcolourfiles);
 unset($clubcodesfound);
 unset($allclubscodeslist);
 unset($paddlerclubs);
