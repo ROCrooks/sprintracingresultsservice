74bd6a63326d886e45f3d36e7e19b51a87b0e987
Richard Crooks <richard@rocrooks.ca>
2024-10-04 21:17:58 -0400

    Make a list of clubs in the database

--- app/public/pages/club-manager.php
+++ app/public/pages/club-manager.php
@@ -8,5 +8,35 @@ echo "<br>";
 print_r($orphanclubs);
 echo "<br>";
 
-$pagehtml = "<section><p>Test</p></section>";
+//Widths of the columns
+$codewidth = 150;
+$clubnamewidth = 3000;
+$colourswidth = 150;
+
+//Make the table heading
+$pagehtml = "";
+$pagehtml = $pagehtml . '<div style="display: table">';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $codewidth . 'px;"><p>Code</p></div>';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $clubnamewidth . 'px;"><p>Club</p></div>';
+$pagehtml = $pagehtml . '</div>';
+
+foreach($clubdetailsresult as $clubdetails)
+    {
+    $pagehtml = $pagehtml . '<div style="display: table">';
+    $pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $codewidth . 'px;"><p>' . $clubdetails['Code'] . '</p></div>';
+    $pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $clubnamewidth . 'px;"><p>' . $clubdetails['LongName'] . '</p></div>';
+    $pagehtml = $pagehtml . '</div>';
+    }
+
+/*$pagehtml = $pagehtml . '<div style="display: table-cell; width: 20px;"><p>' . $paddlerrace['Position'] . '</p></div>';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: 20px;"><p>' . $paddlerrace['Lane'] . '</p></div>';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: 40px;"><p>' . $paddlerrace['Club'] . '</p></div>';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: 300px;"><p>' . $paddlerrace['Crew'] . '</p></div>';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: 100px;"><p>' . $paddlerrace['Time'] . '</p></div>';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: 40px;"><div class="tooltip"><p>' . $paddlerrace['JSV'] . $paddlerrace['MW'] . $paddlerrace['CK'] . '<span class="tooltiptext">' . $hovertext . '</span></p></div></div>';*/
+
+
+
+
+$pagehtml = "<section>" . $pagehtml . "</section>";
 ?>
