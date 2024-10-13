91737c613a7adf9bbfabc5e518cf6b8c6cf44657
Richard Crooks <richard@rocrooks.ca>
2024-10-13 12:07:41 -0400

    Include club colours in output

--- app/public/pages/edit-club.php
+++ app/public/pages/edit-club.php
@@ -5,8 +5,10 @@ include $engineslocation . 'club-details-engine.php';
 print_r($clubfindresult);
 
 $pagehtml = "<section>";
-
-$pagehtml = $pagehtml . '<img src="' . $clubfindresult['Colours'] . '" width="500">';
+$pagehtml = $pagehtml . '<div style="display: table; width: 100%;">';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: 30%;"><img src="' . $clubfindresult['Colours'] . '" width="100%"></div>';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: 70%;"><p>Club</p></div>';
+$pagehtml = $pagehtml . '</div>';
 
 $pagehtml = $pagehtml . "</section>";
 ?>
