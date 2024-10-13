2d486ddb4eedc48dbebb27bf23ad8062090cd6db
Richard Crooks <richard@rocrooks.ca>
2024-10-13 11:19:13 -0400

    Run the query to get the club details from the database

--- app/public/phpengines/club-details-engine.php
+++ app/public/phpengines/club-details-engine.php
@@ -1,4 +1,23 @@
 <?php
+include_once $engineslocation . 'srrs-required-functions.php';
+
 $clubcode = $_GET['club'];
-echo $clubcode;
+
+//Run SQL query to get club details
+$clubfindsql = "SELECT `LongName`, `ShortName`, `WWW`, `WWWapp` FROM `clubs` WHERE `code` = ? LIMIT 0,1 ";
+$clubfindstmt = dbprepare($srrsdblink,$clubfindsql);
+$clubfindresult = dbexecute($clubfindstmt,$clubcode);
+
+//Make empty array if no club found
+if (count($clubfindresult) == 0)
+    {
+    $clubfindresult[0] = array("LongName"=>"","ShortName"=>"","WWW"=>"","WWWapp"=>0);
+    }
+
+//Make the SQL result a single array
+$clubfindresult = $clubfindresult[0];
+
+print_r($clubfindresult);
+
+//echo $clubcode;
 ?>
