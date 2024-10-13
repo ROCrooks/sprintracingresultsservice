c4f4db9c27ed8cc366d66bec2a251a0db2bac32e
Richard Crooks <richard@rocrooks.ca>
2024-10-13 11:40:16 -0400

    Find the club colours associated with the club code given, and check that a valid club code has been received before searching the database

--- app/public/phpengines/club-details-engine.php
+++ app/public/phpengines/club-details-engine.php
@@ -1,12 +1,21 @@
 <?php
 include_once $engineslocation . 'srrs-required-functions.php';
 
-$clubcode = $_GET['club'];
-
-//Run SQL query to get club details
-$clubfindsql = "SELECT `LongName`, `ShortName`, `WWW`, `WWWapp` FROM `clubs` WHERE `code` = ? LIMIT 0,1 ";
-$clubfindstmt = dbprepare($srrsdblink,$clubfindsql);
-$clubfindresult = dbexecute($clubfindstmt,$clubcode);
+//Get the club code from the URL if one is specified
+if (isset($_GET['club']) == true)
+    {
+    $clubcode = $_GET['club'];
+    
+    //Run SQL query to get club details
+    $clubfindsql = "SELECT `LongName`, `ShortName`, `WWW`, `WWWapp` FROM `clubs` WHERE `code` = ? LIMIT 0,1 ";
+    $clubfindstmt = dbprepare($srrsdblink,$clubfindsql);
+    $clubfindresult = dbexecute($clubfindstmt,$clubcode);    
+    }
+else
+    {
+    $clubcode = "";
+    $clubfindresult = array();
+    }
 
 //Make empty array if no club found
 if (count($clubfindresult) == 0)
@@ -17,7 +26,17 @@ if (count($clubfindresult) == 0)
 //Make the SQL result a single array
 $clubfindresult = $clubfindresult[0];
 
-print_r($clubfindresult);
+//Add the club code to the result array
+$clubfindresult['Code'] = $clubcode;
 
-//echo $clubcode;
+//Get the club colours file, or a plain colours if not found
+$coloursfile = $clubcoloursfileslocation . $clubfindresult['Code'] . ".png";
+if (file_exists($coloursfile) == true)
+    $coloursfile = "../clubcolours/" . $clubfindresult['Code'] . ".png";
+else
+    $coloursfile = "../clubcolours/" . "unknown.png";
+
+$clubfindresult['Colours'] = $coloursfile;
+
+print_r($clubfindresult);
 ?>
