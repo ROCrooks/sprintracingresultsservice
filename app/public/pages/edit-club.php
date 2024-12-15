9b9c0a0fc62f008e1e1f976618cda1a48b7f1f93
Richard Crooks <richard@rocrooks.ca>
2024-12-15 12:46:18 -0500

    Make form to update club details

--- app/public/pages/edit-club.php
+++ app/public/pages/edit-club.php
@@ -2,22 +2,26 @@
 //Get the details of the club
 include $engineslocation . 'club-details-engine.php';
 
-print_r($clubfindresult);
-
 $pagehtml = "<section>";
 $pagehtml = $pagehtml . '<div style="display: table; width: 100%; display: flex;">';
 $pagehtml = $pagehtml . '<div style="display: table-cell; width: 30%; display: flex;"><img src="' . $clubfindresult['Colours'] . '" width="100%"></div>';
 
+//Decide if the checkbox for approved URL is set
+if ($clubfindresult['WWWapp'] == 1)
+    $activecheckbox = '<input type="checkbox" name="WWWapp" value= "1" checked>';
+elseif ($clubfindresult['WWWapp'] == 0)
+    $activecheckbox = '<input type="checkbox" name="WWWapp" value= "1">';
 
 //Specify form cell widths
 $labelwidth = 130;
 $formcellwidth = 330;
-//Make the form for changing the club details
 
+//Make the form for changing the club details
+$pagehtml = $pagehtml . '<form id="ClubDetailsForm" method="post" action="EditClub?club=' . $clubfindresult['Code'] . '">';
 $pagehtml = $pagehtml . '<div style="display: table-cell; width: 70%;">';
 $pagehtml = $pagehtml . '<div style="display: table;">';
 $pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $labelwidth . 'px;"><p>Code:</p></div>';
-$pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $formcellwidth . 'px;"><p><input type="text" name="Code" value= "' . $clubfindresult['Code'] . '" size="3"></p></div>';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $formcellwidth . 'px;"><p><input type="text" id="Code" name="Code" value= "' . $clubfindresult['Code'] . '" size="3"></p></div>';
 $pagehtml = $pagehtml . '</div>';
 $pagehtml = $pagehtml . '<div style="display: table;">';
 $pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $labelwidth . 'px;"><p>Long Name:</p></div>';
@@ -29,12 +33,17 @@ $pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $formcellwi
 $pagehtml = $pagehtml . '</div>';
 $pagehtml = $pagehtml . '<div style="display: table;">';
 $pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $labelwidth . 'px;"><p>Website:</p></div>';
-$pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $formcellwidth . 'px;"><p><input type="text" name="WWW" value= "' . $clubfindresult['WWW'] . '" size="30"> Active: <input type="checkbox" name="WWWapp" value= "' . $clubfindresult['WWWapp'] . '"></div>';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $formcellwidth . 'px;"><p><input type="text" name="WWW" value= "' . $clubfindresult['WWW'] . '" size="30"> Active: ' . $activecheckbox .'</div>';
+$pagehtml = $pagehtml . '</div>';
+$pagehtml = $pagehtml . '<div style="display: table;">';
+$pagehtml = $pagehtml . '<div style="display: table-cell; width: ' . $labelwidth+$formcellwidth . 'px;"><p><input type="submit" name="Submit" value="Submit"></p></div>';
 $pagehtml = $pagehtml . '</div>';
 
 $pagehtml = $pagehtml . '</div>';
 
 $pagehtml = $pagehtml . '</div>';
+
+$pagehtml = $pagehtml . '</form>';
 
 $pagehtml = $pagehtml . "</section>";
 ?>
