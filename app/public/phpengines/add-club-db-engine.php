3d5ce0487589040d0cf59b6ee9c8554999fef03f
Richard Crooks <richard@rocrooks.ca>
2025-01-31 22:30:56 -0500

    Check the club code input for length and reject if it is not 3 letters

--- app/public/phpengines/add-club-db-engine.php
+++ app/public/phpengines/add-club-db-engine.php
@@ -1,5 +1,12 @@
 <?php
+//Set validation flag
+$inputerrors = array();
+
+//Check that club code is valid
+if (strlen($clubaddfields['Code']) <> 3)
+    array_push($inputerrors,"Club code is the wrong length");
+
 //Create and run query to add club name
-$addnewclubsql = "INSERT INTO `clubs` (`Code`, `ShortName`, `LongName`, `WWW`, `WWWApp`) VALUES (?, ?, ?, ?, ?)";
-dbprepareandexecute($srrsdblink,$addnewclubsql,$clubaddfields);
+//$addnewclubsql = "INSERT INTO `clubs` (`Code`, `ShortName`, `LongName`, `WWW`, `WWWApp`) VALUES (?, ?, ?, ?, ?)";
+//dbprepareandexecute($srrsdblink,$addnewclubsql,$clubaddfields);
 ?>
