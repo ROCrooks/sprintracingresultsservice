76ab933abfa58d4841ae9784acfad6f3f8e39356
Richard Crooks <richard@rocrooks.ca>
2025-02-19 16:33:33 -0500

    Get the number of paddlers, races, and regattas for the club

--- app/public/phpengines/club-details-engine.php
+++ app/public/phpengines/club-details-engine.php
@@ -1,6 +1,10 @@
 <?php
 include_once $engineslocation . 'srrs-required-functions.php';
 
+//Default the getting the club metrics flag to be false
+if (isset($clubmetrics) == false)
+    $clubmetrics = false;
+
 if ($clubcode != '')
     {
     //Run SQL query to get club details
@@ -9,12 +13,34 @@ if ($clubcode != '')
     $clubfindresult = dbexecute($clubfindstmt,$clubcode);
     }
 else
-    $clubfindresult = array();    
+    $clubfindresult = array();
 
 //Make empty array if no club found
 if (count($clubfindresult) == 0)
     {
     $clubfindresult[0] = array("LongName"=>"","ShortName"=>"","WWW"=>"","WWWapp"=>0);
+    }
+elseif ($clubmetrics == true)
+    {
+    //Count the number of boats with this club in the SRRS database
+    
+    //Make a wildcard version of the club code
+    $wildcardclub = "%" . $clubcode . "%";
+    
+    //Make the queries
+    $countpaddlerssql = "SELECT COUNT(1) FROM `paddlers` WHERE `Club` LIKE ?";
+    $countracessql = "SELECT COUNT(DISTINCT `Race`) FROM `paddlers` WHERE `Club` LIKE ?";
+    $countregattassql = "SELECT COUNT(DISTINCT r.`Regatta`) FROM `races` r INNER JOIN `paddlers` p ON p.`Race`=r.`Key` WHERE `Club` LIKE ?";
+    
+    //Prepare and execute the queries to get the club metrics
+    $countpaddlersresult = dbprepareandexecute($srrsdblink,$countpaddlerssql,$wildcardclub);
+    $countracesresult = dbprepareandexecute($srrsdblink,$countracessql,$wildcardclub);
+    $countregattasresult = dbprepareandexecute($srrsdblink,$countregattassql,$wildcardclub);
+
+    //Make the club metrics readable
+    $clubpaddlers = sqlcountoutput($countpaddlersresult);
+    $clubraces = sqlcountoutput($countracesresult);
+    $clubregattas = sqlcountoutput($countregattasresult);
     }
 
 //Make the SQL result a single array
