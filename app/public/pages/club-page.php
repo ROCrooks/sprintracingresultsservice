acf4418b82097af1cc848dc8605c3ed7c555a027
Richard Crooks <richard@rocrooks.ca>
2025-04-29 14:44:05 -0400

    Club page debugging

--- app/public/pages/club-page.php
+++ app/public/pages/club-page.php
@@ -7,13 +7,23 @@ $clubmetrics = true;
 $clubcode = $club;
 include $engineslocation . 'club-details-engine.php';
 
+$pagehtml = "";
+
+if ($clubfound == true)
+  {
+  //Make the HTML for the club details
+  $pagehtml = $pagehtml . '<section>';
+  $pagehtml = $pagehtml . "<p>Results found for " . $clubfindresult['ShortName'] . "</p>";
+  $pagehtml = $pagehtml . '</section>';
+  }
+
 //List of links
 $subsectionurls = array();
 $subsectionurls[0] = array("URL"=>"RegattaList?club=" . $club,"Text"=>"Browse Regattas");
 $subsectionurls[1] = array("URL"=>"EventRecords?club=" . $club,"Text"=>"Club Records");
 $subsectionurls[2] = array("URL"=>"RegattaAnalytics?club=" . $club,"Text"=>"Regatta Analytics");
 
-$pagehtml = '<section>';
+$pagehtml = $pagehtml . '<section>';
 foreach($subsectionurls as $listurl)
   {
   $pagehtml = $pagehtml . '<p><a href="' . $listurl['URL'] . '">' . $listurl['Text'] . '</a></p>';
