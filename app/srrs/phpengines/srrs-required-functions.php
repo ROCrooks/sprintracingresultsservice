95c70617bb5b0da719a4eefe50e313d57f6e76dd
Richard Crooks <richard@rocrooks.ca>
2025-08-21 13:33:21 -0400

    Update location of the SRRS and general functions in the new file ordering system

--- app/srrs/phpengines/srrs-required-functions.php
+++ app/srrs/phpengines/srrs-required-functions.php
@@ -5,18 +5,18 @@ $generalfiles = array("userinput","db","array","drawgraph","document");
 //Include each required functions file
 foreach ($generalfiles as $file)
   {
-  $url = $generalfunctionsfolder . "/" . $file . "-functions.php";
+  $url = $generalfunctionsfolder . $file . "-functions.php";
   include_once $url;
   }
 
 //Also include the specific SRRS functions
-include_once $srrsfunctionsfolder . "/srrs-functions.php";
+include_once $srrsfunctionsfolder . "srrs-functions.php";
 
 //Create database connection
 //Only create if not already created
 if (isset($srrsdblink) == false)
   {
-  include_once $generalfunctionsfolder . '/dbconnect-function.php';
+  include_once $generalfunctionsfolder . 'dbconnect-function.php';
   $srrsdblink = createdbconnection("SRRS");
   }
 ?>
