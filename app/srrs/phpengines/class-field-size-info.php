c699c8645b45187f90d4fe764de2dab9d5346c93
Richard Crooks <richard@rocrooks.ca>
2026-05-28 16:58:42 -0400

    Separate file with field sizes

--- app/srrs/phpengines/class-field-size-info.php
+++ app/srrs/phpengines/class-field-size-info.php
@@ -1,3 +1,25 @@
 <?php
+//Define cell widths
+$classwidths = array();
+$classwidths['JSV'] = 50;
+$classwidths['MW'] = $classwidths['JSV'];
+$classwidths['CK'] = $classwidths['JSV'];
+$classwidths['Abil'] = 70;
+$classwidths['Spec'] = $classwidths['Abil'];
+$classwidths['Ages'] = $classwidths['Abil'];
+$classwidths['Band'] = 70;
+$classwidths['ShowBand'] = 70;
+$classwidths['FreeText'] = 100;
+$classwidths['Button'] = 60;
 
+//Set field sizes
+$fieldsizes = array();
+$fieldsizes['JSV'] = 2;
+$fieldsizes['MW'] = $fieldsizes['JSV'];
+$fieldsizes['CK'] = $fieldsizes['JSV'];
+$fieldsizes['Abil'] = 6;
+$fieldsizes['Spec'] = 6;
+$fieldsizes['Ages'] = $fieldsizes['Abil'];
+$fieldsizes['Band'] = $fieldsizes['Ages'];
+$fieldsizes['FreeText'] = 40;
 ?>
