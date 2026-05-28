ce260babd3d361de1c6c4c049ddfe0043c313e2b
Richard Crooks <richard@rocrooks.ca>
2026-05-28 16:22:33 -0400

    Include all of the required database functions

--- app/srrs/phpengines/find-single-autoclass.php
+++ app/srrs/phpengines/find-single-autoclass.php
@@ -1,3 +1,13 @@
 <?php
+include_once $srrsenginesfolder . 'srrs-required-functions.php';
 
+//Find the AutoClasses
+if (isset($findclassstmt) == false)
+  {
+  $findclasssql = "SELECT `JSV`, `MW`, `CK`, `Spec`, `Abil`, `Ages`, `Band`, `ShowBand`, `FreeText` FROM `autoclasses` WHERE `RaceName` = ?";
+  $findclassstmt = dbprepare($srrsdblink,$findclasssql);
+  }
+
+//Add the autoclass if it's found
+$autoclass = dbexecute($findclassstmt,$autoclassfind);
 ?>
