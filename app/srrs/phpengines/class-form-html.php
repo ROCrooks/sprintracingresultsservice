3203b575b115f3ba8b68bc5b0a27882d8ac914a4
Richard Crooks <richard@rocrooks.ca>
2026-05-28 16:48:33 -0400

    Move the cell and field sizes to a separate engine

--- app/srrs/phpengines/class-form-html.php
+++ app/srrs/phpengines/class-form-html.php
@@ -25,29 +25,8 @@ $classformhtml = $classformhtml . '<p>Update class name to: <input type="text" n
 $classformhtml = $classformhtml . '<p><input type="submit" name="UpdateInput" value="Update"></p>';
 $classformhtml = $classformhtml . '</form>';
 
-//Define cell widths
-$classwidths = array();
-$classwidths['JSV'] = 50;
-$classwidths['MW'] = $classwidths['JSV'];
-$classwidths['CK'] = $classwidths['JSV'];
-$classwidths['Abil'] = 70;
-$classwidths['Spec'] = $classwidths['Abil'];
-$classwidths['Ages'] = $classwidths['Abil'];
-$classwidths['Band'] = 70;
-$classwidths['ShowBand'] = 70;
-$classwidths['FreeText'] = 100;
-$classwidths['Button'] = 60;
-
-//Set field sizes
-$fieldsizes = array();
-$fieldsizes['JSV'] = 2;
-$fieldsizes['MW'] = $fieldsizes['JSV'];
-$fieldsizes['CK'] = $fieldsizes['JSV'];
-$fieldsizes['Abil'] = 6;
-$fieldsizes['Spec'] = 6;
-$fieldsizes['Ages'] = $fieldsizes['Abil'];
-$fieldsizes['Band'] = $fieldsizes['Ages'];
-$fieldsizes['FreeText'] = 40;
+//Define field and cell sizes
+include $srrsenginesfolder . "class-field-size-info.php";
 
 //Form headings
 $classformhtml = $classformhtml . '<div style="display: table-row;">';
